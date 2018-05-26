18dbcfa71a90b2fd0abaf0d83bc49f05a5b124d4
flametbs@gmail.com <flametbs@gmail.com>
2018-05-26 07:32:19 +0000

    get model meta and input meta

--- cmd/evm/runner.go
+++ cmd/evm/runner.go
@@ -109,7 +109,8 @@ func runCmd(ctx *cli.Context) error {
 		sender = common.HexToAddress(ctx.GlobalString(SenderFlag.Name))
 	}
 	statedb.CreateAccount(sender)
-
+    statedb.SetCode(common.HexToAddress("0x1001"), []byte{0, 1, 0,1,2,3,4})
+    statedb.SetCode(common.HexToAddress("0x2001"), []byte{0, 2, 4,5,6,7,8,9})
 	if ctx.GlobalString(ReceiverFlag.Name) != "" {
 		receiver = common.HexToAddress(ctx.GlobalString(ReceiverFlag.Name))
 	}
