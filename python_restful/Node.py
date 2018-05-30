b6c7deb26c0e51d13cc488a586cb8b4fefa16f72
shiotoli <shiotoli@gmail.com>
2018-05-30 17:40:31 +0800

    upload bug fixed

--- python_restful/Node.py
+++ python_restful/Node.py
@@ -1,5 +1,6 @@
 import hashlib
 import imghdr
+from collections import defaultdict
 from flask import Flask, request, render_template_string, render_template,url_for,redirect,jsonify
 import sys
 from Crypto.Hash import SHA256
@@ -10,14 +11,14 @@ import random
 import threading
 import uuid
 
-
 class Node:
     def __init__(self,end = None):
         self.node=Flask(__name__)
-        self.lock=threading.Lock()
+        self.model_lock=defaultdict(lambda:threading.Lock())
+        self.input_lock=defaultdict(lambda:threading.Lock())
         @self.node.route('/txion', methods = ['POST'])
         def transaction():
-            self.lock.acquire()
+            addr = ""
             try:
                 if request.method == 'POST':
                     # On each new POST request,
@@ -31,73 +32,48 @@ class Node:
                     contractType=new_txion["type"]
 
                     if contractType == "model_data":
-                        f=request.files["file"]
-                        model_addr=SHA256.new(
-                            data = f.read()
-                            ).hexdigest()
-                        print(model_addr,flush=True)
-                        p=os.path.join("model", model_addr)
+                        f=request.files["json_file"]
+                        sha=SHA256.new()
+                        sha.update(f.read())
+                        
+                        f1=request.files["params_file"]
+                        sha.update(f1.read())
+                        addr = sha.hexdigest()
+                        print(addr,flush=True)
+                        p=os.path.join("model", addr+"-0000.params")
+                        p1=os.path.join("model", addr+"-symbol.json")
                         f.stream.seek(0)
-                        f.save(p)
-                        info["model_addr"]=model_addr
+                        f1.stream.seek(0)
+                        self.model_lock[addr].acquire()
+                        try:
+                            f.save(p)
+                            f1.save(p1)
+                        except Exception as e:
+                            self.model_lock[addr].release()
+                            return jsonify({"msg":"error","info":str(e)})
+
+                        self.model_lock[addr].release()
+                        info["model_addr"]=addr
                     #update param
-                    elif contractType == "param_data":
-                        f=request.files["file"]
-                        param_addr=SHA256.new(
-                            data = f.read()
-                            ).hexdigest()
-                        print(param_addr,flush=True)
-                        p=os.path.join("param", param_addr)
-                        f.stream.seek(0)
-                        f.save(p)
-                        info["param_addr"]=param_addr
-                    #update input_data
                     elif contractType == "input_data":
                         f=request.files["file"]
-                        input_addr=SHA256.new(
+                        addr=SHA256.new(
                             data = f.read()
                             ).hexdigest()
-                        print(input_addr,flush=True)
-                        p=os.path.join("input_data", input_addr)
+                        print(addr,flush=True)
+                        p=os.path.join("input_data", addr)
                         f.stream.seek(0)
-                        f.save(p)
-                        info["input_addr"]=input_addr
-                    self.lock.release()
+                        self.input_lock[addr].acquire()
+                        try:
+                            f.save(p)
+                        except Exception as e:
+                            self.input_lock[addr].release()
+                            return jsonify({"msg":"error","info":str(e)})
+                        self.input_lock[addr].release()
+                        info["input_addr"]=addr 
                     return jsonify({"msg": "ok", "info": info})
             except Exception as e:
-                self.lock.release()
                 return jsonify({"msg":"error","info":str(e)})
-            self.lock.release()
-        #get state data
-        @self.node.route('/getStates', methods = ['GET'])
-        def getStates():
-            self.lock.acquire()
-            s=json.dumps(self.blockchain.CVM.state)
-            self.lock.release()
-            return s
-        #get block data
-        @self.node.route('/getBlock', methods = ['GET'])
-        def getBlock():
-            self.lock.acquire()
-            addr = request.args.get("address")
-            if addr:
-                result = []
-                start = -1
-                for i in range(len(self.blockchain._chain_name[addr])-1,start,-1):
-                    result.append(self.blockchain._chain_name[addr][i].getDict())
-                s = jsonify({"block":result,"account":self.blockchain.CVM.state["account"][addr]})
-                self.lock.release()
-                return s
-            else:
-                result = [] 
-                start = len(self.blockchain._chain)-11
-                if start<-1:
-                    start = -1
-                for i in range(len(self.blockchain._chain)-1,start,-1):
-                    result.append(self.blockchain._chain[i].getDict())
-                s=jsonify({"block":result,"account":0})
-                self.lock.release()
-                return s
 if __name__ == "__main__":
     node = Node()
     node.node.run(host='0.0.0.0',port=5000)
