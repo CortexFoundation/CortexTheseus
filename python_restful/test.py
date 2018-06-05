fb4ced7fb2a841b3acdca21d00ef3b3fbab0e81a
flametbs@gmail.com <flametbs@gmail.com>
2018-06-05 09:06:38 +0000

    1. meta tools updated 2. upload function updated

--- python_restful/test.py
+++ python_restful/test.py
@@ -55,16 +55,16 @@ if __name__ == "__main__":
     print(json.dumps(model_info),flush=True)
     input_info = uploadInput(miner,"upload/data").json()
     print(json.dumps(input_info),flush=True)
-    ii = json.loads(input_info["info"])
-    mi = json.loads(model_info["info"])
+    ii = input_info["info"]
+    mi = model_info["info"]
     infer_info = infer(ii["Hash"],mi["Hash"])
     print(infer_info)
     model_info = uploadModel("upload/Inception-BN-symbol-2.json", "upload/Inception-BN-0126.params").json()
     print(json.dumps(model_info),flush=True)
     input_info = uploadInput(miner,"upload/data").json()
     print(json.dumps(input_info),flush=True)
-    ii = json.loads(input_info["info"])
-    mi = json.loads(model_info["info"])
+    ii = input_info["info"]
+    mi = model_info["info"]
     infer_info = infer(ii["Hash"],mi["Hash"])
     print(infer_info)
 
