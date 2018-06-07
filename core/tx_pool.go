f7863290eda204b0e7395676e478b22e5a4a474b
ucwong <@>
2018-06-08 00:18:13 +0800

    add feature: is infer

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -26,6 +26,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/asm"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/event"
@@ -76,6 +77,8 @@ var (
 	// than some meaningful limit a user might use. This is not a consensus error
 	// making the transaction invalid, rather a DOS protection.
 	ErrOversizedData = errors.New("oversized data")
+
+	ErrHasInferOperation = errors.New("has infer operation")
 )
 
 var (
@@ -136,6 +139,7 @@ type TxPoolConfig struct {
 	GlobalQueue  uint64 // Maximum number of non-executable transaction slots for all accounts
 
 	Lifetime time.Duration // Maximum amount of time non-executable transaction are queued
+	NoInfers bool //whether disable infer handling
 }
 
 // DefaultTxPoolConfig contains the default configurations for the transaction
@@ -590,6 +594,11 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 	}
 	if tx.Gas() < intrGas {
 		return ErrIntrinsicGas
+	}
+
+	if pool.config.NoInfers && asm.HasInferOp(tx.Data()) {
+		fmt.Println("Has INFER operation !!!")
+		//return ErrHasInferOperation
 	}
 	return nil
 }
