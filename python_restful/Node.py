34e870364944cbd342e4ddd47872d0686c183618
flametbs@gmail.com <flametbs@gmail.com>
2018-07-05 10:44:25 +0000

    multithreaded infer server

--- python_restful/Node.py
+++ python_restful/Node.py
@@ -1,5 +1,4 @@
 import hashlib
-import imghdr
 from collections import defaultdict
 from flask import Flask, request, render_template_string, render_template,url_for,redirect,jsonify
 import sys
@@ -10,15 +9,16 @@ import json
 import time
 import random
 import threading
-import uuid
-import convertimage2numpy as cn
 import Inference
+from gevent.pywsgi import WSGIServer
+import convertimage2numpy as cn
+from datetime import datetime
 class Node:
     def __init__(self,end = None):
-        self.node=Flask(__name__)
-        self.model_lock=defaultdict(lambda:threading.Lock())
-        self.input_lock=defaultdict(lambda:threading.Lock())
-        self.inferObj = Inference.Inference(json.load(open("config.json","r")))
+        self.node = Flask(__name__)
+        self.model_lock = defaultdict(lambda:threading.Lock())
+        self.input_lock = defaultdict(lambda:threading.Lock())
+        self.inferObj = Inference.InferHelper(json.load(open("config.json","r")))
         @self.node.route('/txion', methods = ['POST'])
         def transaction():
             try:
@@ -53,10 +53,9 @@ class Node:
                             f.save(p)
                             f1.save(p1)
                         except Exception as e:
-                            self.model_lock[addr].release()
                             return jsonify({"msg":"error","info":str(e)})
-
-                        self.model_lock[addr].release()
+                        finally:
+                            self.model_lock[addr].release()
                         # info["model_addr"]=addr
                         info = {"Hash":addr,"AuthorAddress":new_txion["author"],"RawSize":(len(fr1)+len(fr)),"InputShape":[3,224,224],"OutputShape":[1],"Gas": (len(fr1)+len(fr))  / 100}
                     #update param
@@ -74,9 +73,9 @@ class Node:
                         try:
                             np.save(open(p,"wb"),fr)
                         except Exception as e:
-                            self.input_lock[addr].release()
                             return jsonify({"msg":"error","info":str(e)})
-                        self.input_lock[addr].release()
+                        finally:
+                            self.input_lock[addr].release()
                         # info["input_addr"]=addr
                         info = {"Hash":addr,"AuthorAddress":new_txion["author"],"RawSize": len(fr),"Shape":[3,224,224]}
                     return jsonify({"msg": "ok", "info": info})
@@ -87,33 +86,34 @@ class Node:
             try:
                 if request.method == 'POST':
                     new_infer=request.get_json()
-                    print(new_infer)
+                    print(datetime.now(), new_infer, file=sys.stderr)
+                    self.model_lock[new_infer["model_addr"]].acquire()
+                    model_addr = new_infer["model_addr"]
                     if not os.path.exists(os.path.join("model",new_infer["model_addr"]+"-symbol.json")):
                         return jsonify({"msg":"error","info":"json file does not exist"})
                     if not os.path.exists(os.path.join("model",new_infer["model_addr"]+"-0000.params")):
                         return jsonify({"msg":"error","info":"params file does not exist"})
+                    self.model_lock[new_infer["model_addr"]].release()
+
+                    self.input_lock[new_infer["input_addr"]].acquire()
+                    input_addr = new_infer["input_addr"]
                     if not os.path.exists(os.path.join("input_data",new_infer["input_addr"])):
                         return jsonify({"msg":"error","info":"input file does not exist"})
-                    self.input_lock[new_infer["input_addr"]].acquire()
-                    self.model_lock[new_infer["model_addr"]].acquire()
+                    self.input_lock[new_infer["input_addr"]].release()
+
                     try:
-                        res = self.inferObj.testSingleInference(
-                            new_infer["model_addr"],
-                            new_infer["input_addr"]
-                            )
+                        res = self.inferObj.makeInfer(
+                                model_addr,
+                                input_addr
+                        )
                         if res["status"] == "error":
-                            self.input_lock[new_infer["input_addr"]].release()
-                            self.model_lock[new_infer["model_addr"]].release()
                             return jsonify({"msg": "error", "info": res})
                     except Exception as ex:
-                        self.input_lock[new_infer["input_addr"]].release()
-                        self.model_lock[new_infer["model_addr"]].release()
+                        print (datetime.now(), str(ex))
                         return jsonify({"msg":"error","info":str(ex)})
-                    self.input_lock[new_infer["input_addr"]].release()
-                    self.model_lock[new_infer["model_addr"]].release()
                     return jsonify({"msg": "ok", "info": str(res["info"])})
             except Exception as ex:
                 return jsonify({"msg":"error","info":str(ex)})
 if __name__ == "__main__":
     node = Node()
-    node.node.run(host='0.0.0.0', port=5000, threaded=False)
+    node.node.run(host='0.0.0.0', port=5000, threaded=True)
