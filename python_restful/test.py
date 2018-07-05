97715b0fd976aef7a2e31b2b7acba30ed9ea04e6
flametbs@gmail.com <flametbs@gmail.com>
2018-07-05 06:15:12 +0000

    performance improvement required and test rpc server

--- python_restful/test.py
+++ python_restful/test.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import time
 nonce = 0
 miner = "0x0000000000000000000000000000000000000000001"
 def getState():
@@ -18,26 +19,26 @@ def infer(input_addr,model_addr):
 def uploadInput(f,filepath):
     (tt,tempfilename) = os.path.split(filepath)
     files = {
-        'json': ("json", json.dumps({"type":"input_data","author":"0x0553b0185a35cd5bb6386747517ef7e53b15e287","filename":tempfilename}), 'application/json'),
-        'file': (tempfilename, open(filepath, 'rb'), 'application/octet-stream')
-    }
+            'json': ("json", json.dumps({"type":"input_data","author":"0x0553b0185a35cd5bb6386747517ef7e53b15e287","filename":tempfilename}), 'application/json'),
+            'file': (tempfilename, open(filepath, 'rb'), 'application/octet-stream')
+            }
     return requests.post("http://127.0.0.1:5000/txion",files = files)
 def uploadModel(json_filepath,params_file_path):
     (_,json_tempfilename) = os.path.split(json_filepath)
     (_,params_tempfilename) = os.path.split(params_file_path)
     files = {
-        'json': ("json", json.dumps({"type":"model_data","author":"0x0553b0185a35cd5bb6386747517ef7e53b15e287"}), 'application/json'),
-        'json_file': (json_tempfilename, open(json_filepath, 'rb'), 'application/octet-stream'),
-        'params_file': (params_tempfilename, open(params_file_path, 'rb'), 'application/octet-stream')
-    }
+            'json': ("json", json.dumps({"type":"model_data","author":"0x0553b0185a35cd5bb6386747517ef7e53b15e287"}), 'application/json'),
+            'json_file': (json_tempfilename, open(json_filepath, 'rb'), 'application/octet-stream'),
+            'params_file': (params_tempfilename, open(params_file_path, 'rb'), 'application/octet-stream')
+            }
     return requests.post("http://127.0.0.1:5000/txion",files = files)
 def uploadParam(f,filepath):
     (tt,tempfilename) = os.path.split(filepath)
     headers = {'Content-type': 'multipart/form-data'}
     files = {
-        'json': ("json", json.dumps({"type":"param_data","filename":tempfilename}), 'application/json'),
-        'file': (tempfilename, open(filepath, 'rb'), 'application/octet-stream')
-    }
+            'json': ("json", json.dumps({"type":"param_data","filename":tempfilename}), 'application/json'),
+            'file': (tempfilename, open(filepath, 'rb'), 'application/octet-stream')
+            }
     return requests.post("http://127.0.0.1:5000/txion",files = files)
 def createContract(f,model_address,param_address):
     global nonce
@@ -51,17 +52,18 @@ def callContract(f,input_address,contract_address):
 if __name__ == "__main__":
     with open('gistfile1.txt', 'r') as fin:
         label_dict = json.loads("".join(fin.readlines()).encode())
-    print (label_dict)
     model_info = uploadModel("upload/Inception-BN-symbol.json", "upload/Inception-BN-0126.params").json()
     print(json.dumps(model_info),flush=True)
     dataset = ['upload/testing_machine.JPG']
-    dataset += [ 'upload/bird/%d.jpeg' %x for x in range(1, 6)]
-    dataset += [ 'upload/cock/%d.jpeg' % x for x in range(1, 5)]
-    dataset += [ 'upload/duck/%d.jpeg' % x for x in range(1, 5)]
-
+    #dataset += [ 'upload/bird/%d.jpeg' %x for x in range(1, 6)]
+    #dataset += [ 'upload/cock/%d.jpeg' % x for x in range(1, 5)]
+    #dataset += [ 'upload/duck/%d.jpeg' % x for x in range(1, 5)]
+    start_time = time.time()
     for data in dataset:
         input_info = uploadInput(miner, data).json()
         ii = input_info["info"]
         mi = model_info["info"]
-        infer_info = infer(ii["Hash"],mi["Hash"])
+        for i in range(100):
+            infer_info = infer(ii["Hash"],mi["Hash"])
         print(data, label_dict[infer_info['info']], infer_info, json.dumps(input_info))
+    print (time.time() - start_time)
