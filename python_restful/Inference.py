93cea624ee08e1f6542b2f937259d12974530493
flametbs@gmail.com <flametbs@gmail.com>
2018-07-07 10:03:37 +0000

    added interface for list model/input, account management mod required

--- python_restful/Inference.py
+++ python_restful/Inference.py
@@ -12,8 +12,9 @@ import random
 import threading
 from multiprocessing import Pool, Pipe, Process
 
+
 class Inference:
-    def __init__(self,config):
+    def __init__(self, config):
         self.batchsize = config["batchsize"]
         self.model_names = config["models"]
         self.img_dir = config["img_dir"]
@@ -22,58 +23,69 @@ class Inference:
         self.models = config["models"]
         self.model_list = {}
         self.lock = threading.Lock()
-        print ('Inference init')
+        print('Inference init')
 
     #load model
-    def loadModel(self,name,type="cpu",gpu_id = 0):
+    def loadModel(self, name, type="cpu", gpu_id=0):
         begin = time.time()
         sym, arg_params, aux_params = mx.model.load_checkpoint(name, 0)
-        mod = mx.mod.Module(symbol=sym, context=mx.cpu(), label_names=None) if type=="cpu" else mx.mod.Module(symbol=sym, context=mx.gpu(gpu_id), label_names=None)
-        mod.bind(for_training=False, data_shapes=[('data', (self.batchsize, 3, 224, 224))])
+        mod = mx.mod.Module(
+            symbol=sym, context=mx.cpu(),
+            label_names=None) if type == "cpu" else mx.mod.Module(
+                symbol=sym, context=mx.gpu(gpu_id), label_names=None)
+        mod.bind(
+            for_training=False,
+            data_shapes=[('data', (self.batchsize, 3, 224, 224))])
         mod.set_params(arg_params, aux_params, allow_missing=True)
         end = time.time()
-        return mod,end-begin
+        return mod, end - begin
 
     #predict single image
-    def predictSingle(self,name,mod):
+    def predictSingle(self, name, mod):
         imgs = []
-        img = np.load(os.path.join(self.img_dir,name))
+        img = np.load(os.path.join(self.img_dir, name))
         imgs.append(img)
         Batch = namedtuple('Batch', ['data'])
         begin = time.time()
         mod.forward(Batch([mx.nd.array(imgs)]))
         prob = mod.get_outputs()[0].asnumpy()
         end = time.time()
-        return np.argmax(prob), end-begin
+        return np.argmax(prob), end - begin
 
     #get file size
-    def getFileSize(self,filePath):
+    def getFileSize(self, filePath):
         # filePath = unicode(filePath,'utf8')
         fsize = os.path.getsize(filePath)
-        fsize = fsize/float(1024*1024)
-        return round(fsize,2)
+        fsize = fsize / float(1024 * 1024)
+        return round(fsize, 2)
 
     #test single inference
-    def testSingleInference(self,model,data,t="cpu"):
+    def testSingleInference(self, model, data, t="cpu"):
         self.lock.acquire()
         try:
             if model not in self.model_list.keys():
-                mod = self.loadModel(os.path.join(self.model_dir,model),type=t)
+                mod = self.loadModel(
+                    os.path.join(self.model_dir, model), type=t)
                 self.model_list[model] = mod[0]
-            return {"status":"ok","info":self.predictSingle(data,self.model_list[model])[0]}
+            return {
+                "status": "ok",
+                "info": self.predictSingle(data, self.model_list[model])[0]
+            }
         except Exception as ex:
-            return {"status":"error","info":str(ex)}
+            return {"status": "error", "info": str(ex)}
         finally:
             self.lock.release()
 
+
 class InferHelper(object):
-    def __init__(self, config, n_jobs = 10):
+    def __init__(self, config, n_jobs=10):
         self.workers = []
         self.msg_queues = []
         for i in range(n_jobs):
             parent_conn, child_conn = Pipe()
             self.msg_queues.append(parent_conn)
-            self.workers.append(Process(target=self.__handler, args=(config, child_conn, i)))
+            self.workers.append(
+                Process(target=self.__handler, args=(config, child_conn, i)))
         for p in self.workers:
             p.start()
         self.q_lock = threading.Lock()
@@ -125,7 +137,7 @@ class InferHelper(object):
         inferObj = Inference(config)
         while True:
             model, data, t = q.recv()
-            print ('Process %d: '% idx, model, data, t, file=sys.stderr)
+            print('Process %d: ' % idx, model, data, t, file=sys.stderr)
             res = inferObj.testSingleInference(model, data, t)
             q.send(res)
 
@@ -133,9 +145,10 @@ class InferHelper(object):
         for p in self.workers:
             p.join()
 
+
 if __name__ == "__main__":
-    inferObj = Inference(json.load(open("config.json","r")))
+    inferObj = Inference(json.load(open("config.json", "r")))
     res = inferObj.testSingleInference(
-            "5c4d1f84063be8e25e83da6452b1821926548b3c2a2a903a0724e14d5c917b00",
-            "e23438003ce13f86ff46e41441d568dfc6b8a7279a52a2f0b01c3aa5ed85c2c6")
+        "5c4d1f84063be8e25e83da6452b1821926548b3c2a2a903a0724e14d5c917b00",
+        "e23438003ce13f86ff46e41441d568dfc6b8a7279a52a2f0b01c3aa5ed85c2c6")
     print(res)
--- python_restful/Node.py
+++ python_restful/Node.py
@@ -1,6 +1,6 @@
 import hashlib
 from collections import defaultdict
-from flask import Flask, request, render_template_string, render_template,url_for,redirect,jsonify
+from flask import Flask, request, render_template_string, render_template, url_for, redirect, jsonify
 import sys
 from Crypto.Hash import SHA256
 import numpy as np
@@ -13,11 +13,18 @@ import Inference
 from gevent.pywsgi import WSGIServer
 import convertimage2numpy as cn
 from datetime import datetime
+
+
 class Node:
-    def __init__(self,end = None):
-        self.model_lock = defaultdict(lambda:threading.Lock())
-        self.input_lock = defaultdict(lambda:threading.Lock())
-        self.inferObj = Inference.InferHelper(json.load(open("config.json","r")))
+    def __init__(self, end=None):
+        self.model_lock = defaultdict(lambda: threading.Lock())
+        self.input_lock = defaultdict(lambda: threading.Lock())
+        self.inferObj = Inference.InferHelper(
+            json.load(open("config.json", "r")))
+        self.input_list_lock = threading.Lock()
+        self.input_list = {}
+        self.model_list_lock = threading.Lock()
+        self.model_list = {}
 
     def transaction(self, request):
         try:
@@ -25,26 +32,34 @@ class Node:
                 # On each new POST request,
                 # we extract the transaction data
 
-                new_txion=json.loads(request.files["json"].read())
+                new_txion = json.loads(request.files["json"].read())
                 # Then we add the transaction to our list
                 # Because the transaction was successfully
                 # submitted, we log it to our console
-                info={}
-                contractType=new_txion["type"]
+                info = {}
+                contractType = new_txion["type"]
 
-                print(contractType,flush=True)
+                print(contractType, flush=True)
                 if contractType == "model_data":
-                    f=request.files["params_file"]
-                    sha=SHA256.new()
+                    f = request.files["params_file"]
+                    sha = SHA256.new()
                     fr = f.read()
                     sha.update(fr)
 
-                    f1=request.files["json_file"]
+                    f1 = request.files["json_file"]
                     fr1 = f1.read()
                     sha.update(fr1)
-                    addr = "0x"+sha.hexdigest()
-                    p=os.path.join("model", addr+"-0000.params")
-                    p1=os.path.join("model", addr+"-symbol.json")
+                    addr = "0x" + sha.hexdigest()
+                    info = {
+                        "Hash": addr,
+                        "AuthorAddress": new_txion["author"],
+                        "RawSize": (len(fr1) + len(fr)),
+                        "InputShape": [3, 224, 224],
+                        "OutputShape": [1],
+                        "Gas": (len(fr1) + len(fr)) / 100
+                    }
+                    p = os.path.join("model", addr + "-0000.params")
+                    p1 = os.path.join("model", addr + "-symbol.json")
                     f.stream.seek(0)
                     f1.stream.seek(0)
                     self.model_lock[addr].acquire()
@@ -52,75 +67,109 @@ class Node:
                         f.save(p)
                         f1.save(p1)
                     except Exception as e:
-                        return jsonify({"msg":"error","info":str(e)})
+                        return jsonify({"msg": "error", "info": str(e)})
                     finally:
                         self.model_lock[addr].release()
                     # info["model_addr"]=addr
-                    info = {"Hash":addr,"AuthorAddress":new_txion["author"],"RawSize":(len(fr1)+len(fr)),"InputShape":[3,224,224],"OutputShape":[1],"Gas": (len(fr1)+len(fr))  / 100}
+                    self.model_list_lock.acquire()
+                    self.model_list[addr] = info
+                    self.model_list_lock.release()
                 #update param
+                elif contractType == "list_input":
+                    self.input_list_lock.acquire()
+                    resp = jsonify({"msg": "ok", "info": self.input_list})
+                    self.input_list_lock.release()
+                    return resp
+                elif contractType == "list_model":
+                    self.model_list_lock.acquire()
+                    resp = jsonify({"msg": "ok", "info": self.model_list})
+                    self.model_list_lock.release()
+                    return resp
                 elif contractType == "input_data":
-                    f=request.files["file"]
+                    f = request.files["file"]
                     fr = f.read()
                     fr = cn.getImageFromFile(fr)
-                    addr="0x"+SHA256.new(
-                        data = fr.tobytes()
-                        ).hexdigest()
-                    print(addr,flush=True)
-                    p=os.path.join("input_data", addr)
+                    addr = "0x" + SHA256.new(data=fr.tobytes()).hexdigest()
+                    info = {
+                        "Hash": addr,
+                        "AuthorAddress": new_txion["author"],
+                        "RawSize": len(fr),
+                        "Shape": [3, 224, 224]
+                    }
+                    print(addr, flush=True)
+                    p = os.path.join("input_data", addr)
                     f.stream.seek(0)
                     self.input_lock[addr].acquire()
                     try:
-                        np.save(open(p,"wb"),fr)
+                        np.save(open(p, "wb"), fr)
                     except Exception as e:
-                        return jsonify({"msg":"error","info":str(e)})
+                        return jsonify({"msg": "error", "info": str(e)})
                     finally:
                         self.input_lock[addr].release()
+                    self.input_list_lock.acquire()
+                    self.input_list[addr] = info
+                    self.input_list_lock.release()
                     # info["input_addr"]=addr
-                    info = {"Hash":addr,"AuthorAddress":new_txion["author"],"RawSize": len(fr),"Shape":[3,224,224]}
                 return jsonify({"msg": "ok", "info": info})
         except Exception as e:
-            return jsonify({"msg":"error","info":str(e)})
+            return jsonify({"msg": "error", "info": str(e)})
+
     def infer(self, request):
         try:
             if request.method == 'POST':
-                new_infer=request.get_json()
+                new_infer = request.get_json()
                 print(datetime.now(), new_infer, file=sys.stderr)
                 self.model_lock[new_infer["model_addr"]].acquire()
                 model_addr = new_infer["model_addr"]
-                if not os.path.exists(os.path.join("model",new_infer["model_addr"]+"-symbol.json")):
-                    return jsonify({"msg":"error","info":"json file does not exist"})
-                if not os.path.exists(os.path.join("model",new_infer["model_addr"]+"-0000.params")):
-                    return jsonify({"msg":"error","info":"params file does not exist"})
+                if not os.path.exists(
+                        os.path.join(
+                            "model",
+                            new_infer["model_addr"] + "-symbol.json")):
+                    return jsonify({
+                        "msg": "error",
+                        "info": "json file does not exist"
+                    })
+                if not os.path.exists(
+                        os.path.join(
+                            "model",
+                            new_infer["model_addr"] + "-0000.params")):
+                    return jsonify({
+                        "msg": "error",
+                        "info": "params file does not exist"
+                    })
                 self.model_lock[new_infer["model_addr"]].release()
 
                 self.input_lock[new_infer["input_addr"]].acquire()
                 input_addr = new_infer["input_addr"]
-                if not os.path.exists(os.path.join("input_data",new_infer["input_addr"])):
-                    return jsonify({"msg":"error","info":"input file does not exist"})
+                if not os.path.exists(
+                        os.path.join("input_data", new_infer["input_addr"])):
+                    return jsonify({
+                        "msg": "error",
+                        "info": "input file does not exist"
+                    })
                 self.input_lock[new_infer["input_addr"]].release()
 
                 try:
-                    res = self.inferObj.makeInfer(
-                            model_addr,
-                            input_addr
-                    )
+                    res = self.inferObj.makeInfer(model_addr, input_addr)
                     if res["status"] == "error":
                         return jsonify({"msg": "error", "info": res})
                 except Exception as ex:
-                    print (datetime.now(), str(ex))
-                    return jsonify({"msg":"error","info":str(ex)})
+                    print(datetime.now(), str(ex))
+                    return jsonify({"msg": "error", "info": str(ex)})
                 return jsonify({"msg": "ok", "info": str(res["info"])})
         except Exception as ex:
-            return jsonify({"msg":"error","info":str(ex)})
+            return jsonify({"msg": "error", "info": str(ex)})
+
 
 if __name__ == "__main__":
     node = Node()
     server = Flask(__name__)
-    @server.route('/txion', methods = ['POST'])
+
+    @server.route('/txion', methods=['POST'])
     def txion():
         return node.transaction(request)
 
-    @server.route('/infer', methods = ['POST'])
+    @server.route('/infer', methods=['POST'])
     def infer():
         return node.infer(request)
 
