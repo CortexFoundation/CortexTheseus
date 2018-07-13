58e356e81c0c5c2e3b4a8dc9967faa1964aaa30c
ucwong <ucwong@126.com>
2018-07-13 20:19:36 +0800

    512k uploading block size

--- core/state_transition.go
+++ core/state_transition.go
@@ -256,7 +256,7 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 	st.state.AddBalance(st.evm.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(gu), st.gasPrice))
 	//todo change upload
 	if st.uploading() {
-		st.state.SubUpload(st.to(), new(big.Int).SetUint64(3*1024*1024))
+		st.state.SubUpload(st.to(), new(big.Int).SetUint64(1*512*1024))
 	}
 
 	return ret, st.gasUsed(), vmerr != nil, err
