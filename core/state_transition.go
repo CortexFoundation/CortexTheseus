51f5a8c17b7401305a89103dd2c8d3a70f32722d
ucwong <ucwong@126.com>
2018-07-19 16:07:53 +0800

    if not

--- core/state_transition.go
+++ core/state_transition.go
@@ -256,7 +256,7 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 	//todo change upload
 	if st.uploading() {
 		st.state.SubUpload(st.to(), new(big.Int).SetUint64(1*512*1024)) //512 bytes
-		if st.state.Uploading(st.to()) {
+		if !st.state.Uploading(st.to()) {
 			//todo
 			st.state.Download(st.to())
 		}
