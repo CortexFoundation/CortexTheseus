2761f321707602c4bd53cc764e3d3820c5591c0e
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-07-30 19:33:04 +0800

    add cuckoo verify interface，stash

---
+++ build/setenv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Curkoo algorithm dynamic library path
+OS=`uname`
+ROOT_DIR=`pwd`
+
+if [ "$OS" == "Linux" ]; then
+	export LD_LIBRARY_PATH=$ROOT_DIR/CortexMiner
+elif [ "$OS" == "Darwin" ]; then
+	export DYLD_LIBRARY_PATH=$ROOT_DIR/CortexMiner
+	echo $DYLD_LIBRARY_PATH
+else
+	echo "Sorry, cannot detected your OS" $OS
+fi
+
+export CGO_CFLAGS=-I$ROOT_DIR/CortexMiner/
+export CGO_LDFLAGS=-L$ROOT_DIR/CortexMiner/
