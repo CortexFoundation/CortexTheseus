e6973601844321f9299532af41661a3e07531934
shiotoli <shiotoli@gmail.com>
2018-08-10 19:35:52 +0800

    add solution hash

--- core/types/block.go
+++ core/types/block.go
@@ -43,6 +43,7 @@ var (
 // out on a block.
 type BlockNonce [8]byte
 type BlockSolution [42]uint32
+type BlockSolutionHash [8]byte
 
 // EncodeNonce converts the given integer to a block nonce.
 func EncodeNonce(i uint64) BlockNonce {
@@ -77,27 +78,39 @@ func (s BlockSolution) UnmarshalText(input []byte) error {
 	err := binary.Read(rbuf, binary.LittleEndian, &s)
 	return err
 }
+func (s BlockSolutionHash) MarshalText() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, s)
+	return buf.Bytes(), err
+}
+
+func (s BlockSolutionHash) UnmarshalText(input []byte) error {
+	rbuf := bytes.NewReader(input)
+	err := binary.Read(rbuf, binary.LittleEndian, &s)
+	return err
+}
 
 //go:generate gencodec -type Header -field-override headerMarshaling -out gen_header_json.go
 
 // Header represents a block header in the Ethereum blockchain.
 type Header struct {
-	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
-	UncleHash   common.Hash    `json:"sha3Uncles"       gencodec:"required"`
-	Coinbase    common.Address `json:"miner"            gencodec:"required"`
-	Root        common.Hash    `json:"stateRoot"        gencodec:"required"`
-	TxHash      common.Hash    `json:"transactionsRoot" gencodec:"required"`
-	ReceiptHash common.Hash    `json:"receiptsRoot"     gencodec:"required"`
-	Bloom       Bloom          `json:"logsBloom"        gencodec:"required"`
-	Difficulty  *big.Int       `json:"difficulty"       gencodec:"required"`
-	Number      *big.Int       `json:"number"           gencodec:"required"`
-	GasLimit    uint64         `json:"gasLimit"         gencodec:"required"`
-	GasUsed     uint64         `json:"gasUsed"          gencodec:"required"`
-	Time        *big.Int       `json:"timestamp"        gencodec:"required"`
-	Extra       []byte         `json:"extraData"        gencodec:"required"`
-	MixDigest   common.Hash    `json:"mixHash"          gencodec:"required"`
-	Nonce       BlockNonce     `json:"nonce"            gencodec:"required"`
-	Solution    BlockSolution  `json:"solution"			gencodec:"required"`
+	ParentHash   common.Hash       `json:"parentHash"       gencodec:"required"`
+	UncleHash    common.Hash       `json:"sha3Uncles"       gencodec:"required"`
+	Coinbase     common.Address    `json:"miner"            gencodec:"required"`
+	Root         common.Hash       `json:"stateRoot"        gencodec:"required"`
+	TxHash       common.Hash       `json:"transactionsRoot" gencodec:"required"`
+	ReceiptHash  common.Hash       `json:"receiptsRoot"     gencodec:"required"`
+	Bloom        Bloom             `json:"logsBloom"        gencodec:"required"`
+	Difficulty   *big.Int          `json:"difficulty"       gencodec:"required"`
+	Number       *big.Int          `json:"number"           gencodec:"required"`
+	GasLimit     uint64            `json:"gasLimit"         gencodec:"required"`
+	GasUsed      uint64            `json:"gasUsed"          gencodec:"required"`
+	Time         *big.Int          `json:"timestamp"        gencodec:"required"`
+	Extra        []byte            `json:"extraData"        gencodec:"required"`
+	MixDigest    common.Hash       `json:"mixHash"          gencodec:"required"`
+	Nonce        BlockNonce        `json:"nonce"            gencodec:"required"`
+	Solution     BlockSolution     `json:"solution"			gencodec:"required"`
+	SolutionHash BlockSolutionHash `json:"solutionHash" 	gencodec:"required"`
 }
 
 // field type overrides for gencodec
