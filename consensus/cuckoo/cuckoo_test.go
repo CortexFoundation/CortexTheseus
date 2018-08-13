cd51000af51b991e8e3d599af43002a6131f8957
shiotoli <shiotoli@gmail.com>
2018-08-13 15:22:07 +0800

    clean project

--- consensus/cuckoo/cuckoo_test.go
+++ consensus/cuckoo/cuckoo_test.go
@@ -9,19 +9,29 @@ import (
 
 func TestTestMode(t *testing.T) {
 	head := &types.Header{
-		Number:     big.NewInt(1),
-		Difficulty: big.NewInt(100),
+		Number:     big.NewInt(123),
+		Difficulty: big.NewInt(1),
 		Time:       big.NewInt(0),
 	}
-
+	// target := big.NewInt(0)
+	// target.Exp(big.NewInt(2), big.NewInt(256), nil)
+	// target.Sub(target, head.Difficulty)
+	// target.Sub(target, big.NewInt(1))
+	// head.Difficulty = target
 	cuckoo := NewTester()
-	block, err := cuckoo.Seal(nil, types.NewBlockWithHeader(head), nil)
+	nb := types.NewBlockWithHeader(head)
+	block, err := cuckoo.Seal(nil, nb, nil)
 	if err != nil {
 		t.Fatalf("failed to seal block: %v", err)
 	}
-
+	// t.Fatalf("1")
+	// t.Fatal(block.Solution())
+	// t.Fatal(block.Header().SolutionHash)
 	head.Nonce = types.EncodeNonce(block.Nonce())
 	head.Solution = block.Solution()
+	// t.Log(head.Solution)
+	// head.Solution[0] = head.Solution[1]
+	head.SolutionHash = block.Header().SolutionHash
 
 	if err := cuckoo.VerifySeal(nil, head); err != nil {
 		t.Fatalf("unexpected verification error: %v", err)
