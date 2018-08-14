b8ac503d6fd66d3e50ca3d5410391c88b38d5aa9
shiotoli <shiotoli@gmail.com>
2018-08-14 18:10:31 +0800

    add lib

--- build/setenv.sh
+++ build/setenv.sh
@@ -5,13 +5,13 @@ OS=`uname`
 ROOT_DIR=`pwd`
 
 if [[ "$OS" == "Linux" ]]; then
-	export LD_LIBRARY_PATH=$ROOT_DIR/CortexMiner
+	export LD_LIBRARY_PATH=$ROOT_DIR
 elif [[ "$OS" == "Darwin" ]]; then
-	export DYLD_LIBRARY_PATH=$ROOT_DIR/CortexMiner
+	export DYLD_LIBRARY_PATH=$ROOT_DIR
 	echo $DYLD_LIBRARY_PATH
 else
 	echo "Sorry, cannot detected your OS" $OS
 fi
 
-export CGO_CFLAGS=-I$ROOT_DIR/CortexMiner/
-export CGO_LDFLAGS=-L$ROOT_DIR/CortexMiner/
+export CGO_CFLAGS=-I$ROOT_DIR/
+export CGO_LDFLAGS=-L$ROOT_DIR/
