7481a2532e027c83c835dce5153e2b11f7e89968
shiotoli <shiotoli@gmail.com>
2018-08-14 17:20:04 +0800

    add cuckoo

--- consensus/cuckoo/cuckoo_test.go
+++ consensus/cuckoo/cuckoo_test.go
@@ -1,6 +1,7 @@
 package cuckoo
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 
@@ -9,18 +10,21 @@ import (
 
 func TestTestMode(t *testing.T) {
 
+	cuckoo := NewTester()
 	for i := 0; i < 1; i++ {
 		head := &types.Header{
 			Number:     big.NewInt(int64(i)),
-			Difficulty: big.NewInt(1),
+			Difficulty: big.NewInt(2),
 			Time:       big.NewInt(int64(i) * int64(i)),
 		}
+		target := new(big.Int).Div(maxUint256, head.Difficulty).Bytes()
+		fmt.Println(target)
+		// t.Fatal()
 		// target := big.NewInt(0)
 		// target.Exp(big.NewInt(2), big.NewInt(256), nil)
 		// target.Sub(target, head.Difficulty)
 		// target.Sub(target, big.NewInt(1))
 		// head.Difficulty = target
-		cuckoo := NewTester()
 		nb := types.NewBlockWithHeader(head)
 		block, err := cuckoo.Seal(nil, nb, nil)
 		if err != nil {
@@ -34,11 +38,11 @@ func TestTestMode(t *testing.T) {
 		// // t.Log(head.Solution)
 		// // head.Solution[0] = head.Solution[1]
 		// head.SolutionHash = block.Header().SolutionHash
-
+		t.Log(block.Header())
+		t.Fatal()
 		if err := cuckoo.VerifySeal(nil, block.Header()); err != nil {
 			t.Fatalf("unexpected verification error: %v", err)
 		}
-
 	}
 
 }
