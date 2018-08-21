41cfc2ede8e1cb4d383f6c9c584ecca75bed7470
lizhen <nehzilrz@gmail.com>
2018-08-21 18:44:21 +0800

    update

--- fs/fs.go
+++ fs/fs.go
@@ -1,32 +1,37 @@
 package fs
 
 import (
-  "math"
-  "torrent/libtorrent"
+	"math"
+	torrent "torrent/libtorrent"
+
+	_ "../libtorrent"
 )
 
-func Host(torrentFiles []string) (err error)  {
-  flags := &torrent.TorrentFlags{
-    Dial:                nil,
-    Port:                7777,
-    FileDir:             ".",
-    SeedRatio:           math.Inf(0),
-    UseDeadlockDetector: true,
-    UseLPD:              true,
-    UseDHT:              true,
-    UseUPnP:             false,
-    UseNATPMP:           false,
-    TrackerlessMode:     false,
-                        // IP address of gateway
-    Gateway:            "",
-    InitialCheck:       true,
-    FileSystemProvider: torrent.OsFsProvider{},
-    Cacher:             torrent.NewRamCacheProvider(2048),
-    ExecOnSeeding:      "",
-    QuickResume:        true,
-    MaxActive:          16,
-    MemoryPerTorrent:   -1,
-  }
-  torrent.RunTorrents(flags, torrentFiles)
-  return
+func Host(torrentFiles []string) (err error) {
+	StartListening()
+
+	return
+	flags := &torrent.TorrentFlags{
+		Dial:                nil,
+		Port:                7777,
+		FileDir:             "/home/lizhen/storage",
+		SeedRatio:           math.Inf(0),
+		UseDeadlockDetector: true,
+		UseLPD:              true,
+		UseDHT:              true,
+		UseUPnP:             false,
+		UseNATPMP:           false,
+		TrackerlessMode:     false,
+		// IP address of gateway
+		Gateway:            "",
+		InitialCheck:       true,
+		FileSystemProvider: torrent.OsFsProvider{},
+		Cacher:             torrent.NewRamCacheProvider(2048),
+		ExecOnSeeding:      "",
+		QuickResume:        true,
+		MaxActive:          128,
+		MemoryPerTorrent:   -1,
+	}
+	torrent.RunTorrents(flags, torrentFiles)
+	return
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "torrent/fs"
+  "./fs"
 )
 
 func main() {
