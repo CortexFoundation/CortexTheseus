a08bd29f51fd10f11df7fc4ac1455d635da42478
lizhen <nehzilrz@gmail.com>
2018-08-24 19:14:49 +0800

    update

---
+++ monitor/storage.go
@@ -0,0 +1,32 @@
+package monitor
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+
+	download "../manager"
+)
+
+const defaultTorrentName = "1.torrent"
+
+// InitStorage ...
+func InitStorage(storageDir string, manager *download.Manager) {
+	files, err := ioutil.ReadDir(storageDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		infohash := f.Name()
+		if f.IsDir() && len(infohash) == 40 {
+			torrentPath := path.Join(storageDir, f.Name(), defaultTorrentName)
+			if _, err := os.Stat(torrentPath); err == nil {
+				manager.NewTorrent <- torrentPath
+			} else {
+				mURI := "magnet:?xt=urn:btih:" + infohash
+				manager.NewTorrent <- mURI
+			}
+		}
+	}
+}
