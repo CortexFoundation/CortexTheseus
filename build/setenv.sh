a171716834c85b66eb2a413b03a2f32dcb6fdefd
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-08-24 02:00:56 +0000

    Merge and run cuckoo algorithm

--- build/setenv.sh
+++ build/setenv.sh
@@ -3,15 +3,19 @@
 # Curkoo algorithm dynamic library path
 OS=`uname`
 ROOT_DIR=`pwd`
+LIB_MINER_DIR=$ROOT_DIR/libminer
 
 if [[ "$OS" == "Linux" ]]; then
-	export LD_LIBRARY_PATH=$ROOT_DIR
+    export LD_LIBRARY_PATH=$LIB_MINER_DIR/linux/
+    export CGO_CFLAGS=-I$LIB_MINER_DIR/
+    export CGO_LDFLAGS=-L$LIB_MINER_DIR/linux/
+    echo $LD_LIBRARY_PATH
 elif [[ "$OS" == "Darwin" ]]; then
-	export DYLD_LIBRARY_PATH=$ROOT_DIR
+	export DYLD_LIBRARY_PATH=$LIB_MIER_DIR/mac/
+    export CGO_CFLAGS=-I$LIB_MINER_DIR/
+    export CGO_LDFLAGS=-L$LIB_MINER_DIR/mac/
 	echo $DYLD_LIBRARY_PATH
 else
 	echo "Sorry, cannot detected your OS" $OS
 fi
 
-export CGO_CFLAGS=-I$ROOT_DIR/
-export CGO_LDFLAGS=-L$ROOT_DIR/
