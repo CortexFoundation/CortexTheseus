1d1d04f7c7df346e4335808501684d7fc31641ee
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-08-29 10:56:33 +0800

    Add subtree of CortexMiner

--- build/setenv.sh
+++ build/setenv.sh
@@ -3,17 +3,17 @@
 # Curkoo algorithm dynamic library path
 OS=`uname`
 ROOT_DIR=`pwd`
-LIB_MINER_DIR=$ROOT_DIR/libminer
+export LIB_MINER_DIR=$ROOT_DIR/cminer
 
 if [[ "$OS" == "Linux" ]]; then
-    export LD_LIBRARY_PATH=$LIB_MINER_DIR/linux/
+    export LD_LIBRARY_PATH=$LIB_MINER_DIR/
     export CGO_CFLAGS=-I$LIB_MINER_DIR/
-    export CGO_LDFLAGS=-L$LIB_MINER_DIR/linux/
+    export CGO_LDFLAGS=-L$LIB_MINER_DIR/
     echo $LD_LIBRARY_PATH
 elif [[ "$OS" == "Darwin" ]]; then
-	export DYLD_LIBRARY_PATH=$LIB_MIER_DIR/mac/
+	export DYLD_LIBRARY_PATH=$LIB_MINER_DIR/
     export CGO_CFLAGS=-I$LIB_MINER_DIR/
-    export CGO_LDFLAGS=-L$LIB_MINER_DIR/mac/
+    export CGO_LDFLAGS=-L$LIB_MINER_DIR/
 	echo $DYLD_LIBRARY_PATH
 else
 	echo "Sorry, cannot detected your OS" $OS
