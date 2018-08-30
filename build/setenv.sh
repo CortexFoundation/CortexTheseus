64825f718e589c64c29f314e816d59b292b15cdc
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-08-30 08:14:56 +0000

    improve build setenv.sh

--- build/setenv.sh
+++ build/setenv.sh
@@ -6,12 +6,13 @@ ROOT_DIR=`pwd`
 export LIB_MINER_DIR=$ROOT_DIR/cminer
 
 if [[ "$OS" == "Linux" ]]; then
-    export LD_LIBRARY_PATH=$LIB_MINER_DIR/
+    export LD_LIBRARY_PATH=$LIB_MINER_DIR/:$LIB_MINER_DIR/cuckoo
     export CGO_CFLAGS=-I$LIB_MINER_DIR/
-    export CGO_LDFLAGS=-L$LIB_MINER_DIR/
+    export CGO_LDFLAGS=-L$LIB_MINER_DIR/\ -L$LIB_MINER_DIR/cuckoo
     echo $LD_LIBRARY_PATH
+    echo $CGO_LDFLAGS
 elif [[ "$OS" == "Darwin" ]]; then
-	export DYLD_LIBRARY_PATH=$LIB_MINER_DIR/
+	export DYLD_LIBRARY_PATH=$LIB_MINER_DIR/:$LIB_MINER_DIR/cuckoo
     export CGO_CFLAGS=-I$LIB_MINER_DIR/
     export CGO_LDFLAGS=-L$LIB_MINER_DIR/\ -L$LIB_MINER_DIR/cuckoo
 	echo $DYLD_LIBRARY_PATH
