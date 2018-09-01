e04b5bc5bce6d10b458c8e67cd5c80d1096f8ff8
flametbs@gmail.com <flametbs@gmail.com>
2018-09-01 23:52:24 +0800

    stash: successfuly download data via routing by localtracker

--- cmd/monitor/main.go
+++ cmd/monitor/main.go
@@ -1,7 +1,8 @@
 package main
 
-import "../../monitor"
 import (
+	"../../monitor"
+	download "../../manager"
 	"flag"
 	"fmt"
 	"log"
@@ -10,24 +11,36 @@ import (
 
 func main() {
 	flag_addr := flag.String("a", "0.0.0.0:34444", "json-rpc uri")
+	storageDir := flag.String("d", "/home/lizhen/storage", "storage path")
+	trackerURI := flag.String("t", "http://47.52.39.170:5008/announce", "tracker uri")
+	flag.Parse()
+	dlCilent := download.NewTorrentManager(*storageDir)
+	dlCilent.SetTrackers([]string{*trackerURI})
 	m := monitor.NewHttpMonitor(*flag_addr)
 	m.Initilize()
 	strList := make([]string, 0)
 	strMap := make(map[string]interface{})
-	for {
-		time.Sleep(time.Second * 1)
-		log.Println(strList)
-		now := time.Now().UnixNano()
-		strList = append(strList, fmt.Sprintf("%d", time.Now()))
-		if now%2 == 0 {
-			strMap[fmt.Sprintf("%d", now)] = now
-		} else {
-			strMap[fmt.Sprintf("%d", now)] = []string{fmt.Sprintf("%d", now)}
-		}
+	if false {
+		for {
+			time.Sleep(time.Second * 1)
+			log.Println(strList)
+			now := time.Now().UnixNano()
+			strList = append(strList, fmt.Sprintf("%d", time.Now()))
+			if now%2 == 0 {
+				strMap[fmt.Sprintf("%d", now)] = now
+			} else {
+				strMap[fmt.Sprintf("%d", now)] = []string{fmt.Sprintf("%d", now)}
+			}
 
-		m.Update("strList", strList)
-		m.Update("time", time.Now())
-		m.Update("a.b.c", strMap)
+			m.Update("strList", strList)
+			m.Update("time", time.Now())
+			m.Update("a.b.c", strMap)
+		}
+	} else {
+		dlCilent.AddMagnet("magnet:?xt=urn:btih:137B5742C8B4E9CBE64109E8CF5665383ABD8DF3&dn=data&tr=http%3a%2f%2flocalhost%3a8000%2fannounce")
+		for {
+			time.Sleep(time.Second * 1)
+		}
 	}
 	m.Finalize()
 }
