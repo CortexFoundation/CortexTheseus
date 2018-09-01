cc0b3b3d02f6a223be8a9636ded63e05e2f8002e
flametbs@gmail.com <flametbs@gmail.com>
2018-09-01 10:07:33 +0000

    remove dashboard flag

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -62,10 +62,6 @@ var (
 		utils.DataDirFlag,
 		utils.KeyStoreDirFlag,
 		utils.NoUSBFlag,
-		utils.DashboardEnabledFlag,
-		utils.DashboardAddrFlag,
-		utils.DashboardPortFlag,
-		utils.DashboardRefreshFlag,
 		/* utils.EthashCacheDirFlag,
 		utils.EthashCachesInMemoryFlag,
 		utils.EthashCachesOnDiskFlag,
@@ -211,9 +207,6 @@ func init() {
 		runtime.GOMAXPROCS(runtime.NumCPU())
 
 		logdir := ""
-		if ctx.GlobalBool(utils.DashboardEnabledFlag.Name) {
-			logdir = (&node.Config{DataDir: utils.MakeDataDir(ctx)}).ResolvePath("logs")
-		}
 		if err := debug.Setup(ctx, logdir); err != nil {
 			return err
 		}
