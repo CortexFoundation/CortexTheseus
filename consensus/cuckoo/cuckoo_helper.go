a6a5f6a175fa3e4cdea5692bc2e3195b5a38c02c
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-05 14:24:57 +0800

    Add CuckooCycle header

--- consensus/cuckoo/cuckoo_helper.go
+++ consensus/cuckoo/cuckoo_helper.go
@@ -2,18 +2,9 @@ package cuckoo
 
 /*
 #cgo LDFLAGS: -L../../cminer -lgominer -lstdc++
+#cgo CFLAGS: -I../../cminer
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#define uint unsigned int
-	unsigned char CuckooSolve(char *header, uint header_len, uint nonce, uint *result, uint *result_len, unsigned char* target,unsigned char* hash);
-	unsigned char CuckooVerify(char *header, uint header_len, uint nonce, uint *result, unsigned char* target, unsigned char* hash);
-	void CuckooInit(uint nthread);
-	void CuckooFinalize();
-#ifdef __cplusplus
-}
-#endif
+#include "gominer.h"
 */
 import "C"
 import (
