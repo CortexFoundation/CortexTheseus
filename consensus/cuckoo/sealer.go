d54a32739f213092e61fe3b3088348ce10d727d6
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-05 10:42:49 +0800

    Remove source operation for invoking C

--- consensus/cuckoo/sealer.go
+++ consensus/cuckoo/sealer.go
@@ -1,10 +1,5 @@
 package cuckoo
 
-/*
-#cgo LDFLAGS:  -lstdc++ -lgominer
-#include "gominer.h"
-*/
-import "C"
 import (
 	crand "crypto/rand"
 	"errors"
@@ -14,7 +9,6 @@ import (
 	"runtime"
 	"sync"
 	"time"
-	"unsafe"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
@@ -166,15 +160,16 @@ func (cuckoo *Cuckoo) VerifySolution(hash []byte, nonce uint32, solution types.B
 		//result_len uint32
 	)
 	diff := target.Bytes()
-	r := C.CuckooVerify(
-		(*C.char)(unsafe.Pointer(&hash[0])),
-		C.uint(len(hash)),
-		C.uint(uint32(nonce)),
-		(*C.uint)(unsafe.Pointer(&solution[0])),
-		//                        (*C.uint)(unsafe.Pointer(&result_len)),
-		(*C.uchar)(unsafe.Pointer(&diff[0])),
-		(*C.uchar)(unsafe.Pointer(&result_hash[0])))
-	if byte(r) != 0 {
+	r := CuckooVerify(&hash[0], len(hash), uint32(nonce), &solution[0], &diff[0], &result_hash[0])
+	/* r := C.CuckooVerify(
+	(*C.char)(unsafe.Pointer(&hash[0])),
+	C.uint(len(hash)),
+	C.uint(uint32(nonce)),
+	(*C.uint)(unsafe.Pointer(&solution[0])),
+	//                        (*C.uint)(unsafe.Pointer(&result_len)),
+	(*C.uchar)(unsafe.Pointer(&diff[0])),
+	(*C.uchar)(unsafe.Pointer(&result_hash[0]))) */
+	if r != 0 {
 		return true, common.BytesToHash(result_hash[:])
 	}
 	return false, common.BytesToHash(result_hash[:])
@@ -218,19 +213,21 @@ search:
 			var result_hash [32]byte
 			diff := target.Bytes()
 			// cuckoo.cMutex.Lock()
-			r := C.CuckooSolve(
-				(*C.char)(unsafe.Pointer(&hash[0])),
-				C.uint(len(hash)),
-				C.uint(uint32(nonce)),
-				(*C.uint)(unsafe.Pointer(&result[0])),
-				(*C.uint)(unsafe.Pointer(&result_len)),
-				(*C.uchar)(unsafe.Pointer(&diff[0])),
-				(*C.uchar)(unsafe.Pointer(&result_hash[0])))
-			/* if byte(r) == 0 {
+			r := CuckooSolve(&hash[0], len(hash), uint32(nonce), &result[0], &result_len, &diff[0], &result_hash[0])
+			/* r := C.CuckooSolve(
+			(*C.char)(unsafe.Pointer(&hash[0])),
+			C.uint(len(hash)),
+			C.uint(uint32(nonce)),
+			(*C.uint)(unsafe.Pointer(&result[0])),
+			(*C.uint)(unsafe.Pointer(&result_len)),
+			(*C.uchar)(unsafe.Pointer(&diff[0])),
+			(*C.uchar)(unsafe.Pointer(&result_hash[0]))) */
+			if r == 0 {
 				// cuckoo.cMutex.Unlock()
 				nonce++
 				continue
-			} */
+			}
+			r = CuckooVerify(&hash[0], len(hash), uint32(nonce), &result[0], &diff[0], &result_hash[0])
 			/* r = C.CuckooVerify(
 			(*C.char)(unsafe.Pointer(&hash[0])),
 			C.uint(len(hash)),
@@ -240,7 +237,7 @@ search:
 			(*C.uchar)(unsafe.Pointer(&result_hash[0]))) */
 			// cuckoo.cMutex.Unlock()
 
-			if byte(r) != 0 {
+			if r != 0 {
 				// Correct solution found, create a new header with it
 				header = types.CopyHeader(header)
 				header.Nonce = types.EncodeNonce(uint64(nonce))
