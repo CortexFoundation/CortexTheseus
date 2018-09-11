dee7b2b0e6a245314f8c6367c57caa5937320727
ucwong <ucwong@126.com>
2018-09-11 13:41:24 +0800

    clean log

--- consensus/cuckoo/cuckoo_verify.go
+++ consensus/cuckoo/cuckoo_verify.go
@@ -14,7 +14,7 @@ import (
 
 func CuckooVerifyHeaderNonceAndSolutions(hash []byte, nonce uint32, result *uint32) int {
 	tmpHash := hash
-	log.Println("CuckooVerifyHeaderNonceAndSolutions: hash = ", hash, "\nnonce = ", nonce, "\nresult = ", result)
+	//log.Println("CuckooVerifyHeaderNonceAndSolutions: hash = ", hash, "\nnonce = ", nonce, "\nresult = ", result)
 	r := C.CuckooVerifyHeaderNonceAndSolutions(
 		(*C.uchar)(unsafe.Pointer(&tmpHash[0])),
 		C.uint(len(hash)),
