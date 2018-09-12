74294bcc973535d68a5436d85c09514ab5bd5e01
flametbs@gmail.com <flametbs@gmail.com>
2018-09-12 16:12:14 +0000

    modify nonce from uint32 -> uint64; headerNonce protocol updated

--- consensus/cuckoo/cuckoo_helper.go
+++ consensus/cuckoo/cuckoo_helper.go
@@ -23,12 +23,12 @@ func CuckooFinalize() {
 	C.CuckooFinalize()
 }
 
-func CuckooSolve(hash *byte, hash_len int, nonce uint32, result *uint32, result_len *uint32, diff *byte, result_hash *byte) byte {
+func CuckooSolve(hash *byte, hash_len int, nonce uint64, result *uint32, result_len *uint32, diff *byte, result_hash *byte) byte {
 
 	r := C.CuckooSolve(
 		(*C.uint8_t)(unsafe.Pointer(hash)),
 		C.uint32_t(hash_len),
-		C.uint32_t(nonce),
+		C.uint64_t(nonce),
 		(*C.uint32_t)(unsafe.Pointer(result)),
 		(*C.uint32_t)(unsafe.Pointer(result_len)),
 		(*C.uint8_t)(unsafe.Pointer(diff)),
@@ -37,31 +37,34 @@ func CuckooSolve(hash *byte, hash_len int, nonce uint32, result *uint32, result_
 	return byte(r)
 }
 
-func CuckooFindSolutions(hash []byte, nonce uint32, result *[]uint32) (status_code uint32, solLength uint32, numSols uint32) {
+func CuckooFindSolutions(hash []byte, nonce uint64) (status_code uint32, ret [][]uint32) {
 	var (
 		_solLength uint32
 		_numSols   uint32
+		result     [128]uint32
 	)
-	var tmpHash = make([]byte, len(hash))
+	var tmpHash = make([]byte, 32)
 	copy(tmpHash[:], hash)
 	r := C.CuckooFindSolutions(
 		(*C.uint8_t)(unsafe.Pointer(&tmpHash[0])),
-		C.uint32_t(len(tmpHash)),
-		C.uint32_t(nonce),
-		(*C.uint32_t)(unsafe.Pointer(&((*result)[0]))),
-		C.uint32_t(len(*result)),
+		C.uint64_t(nonce),
+		(*C.result_t)(unsafe.Pointer(&result[0])),
+		C.uint32_t(len(result)),
 		(*C.uint32_t)(unsafe.Pointer(&_solLength)),
-		(*C.uint32_t)(unsafe.Pointer(&_numSols)),
-	)
-
-	return uint32(r), _solLength, _numSols
+		(*C.uint32_t)(unsafe.Pointer(&_numSols)))
+	for solIdx := uint32(0); solIdx < _numSols; solIdx++ {
+		var sol = make([]uint32, _solLength)
+		copy(sol[:], result[solIdx*_solLength:(solIdx+1)*_solLength])
+		ret = append(ret, sol)
+	}
+	return uint32(r), ret
 }
 
-func CuckooVerify(hash *byte, hash_len int, nonce uint32, result *uint32, diff *byte, result_hash *byte) byte {
+func CuckooVerify(hash *byte, hash_len int, nonce uint64, result *uint32, diff *byte, result_hash *byte) byte {
 	r := C.CuckooVerify(
 		(*C.uint8_t)(unsafe.Pointer(hash)),
 		C.uint32_t(hash_len),
-		C.uint32_t(uint32(nonce)),
+		C.uint64_t(nonce),
 		(*C.result_t)(unsafe.Pointer(result)),
 		(*C.uint8_t)(unsafe.Pointer(diff)),
 		(*C.uint8_t)(unsafe.Pointer(result_hash)))
