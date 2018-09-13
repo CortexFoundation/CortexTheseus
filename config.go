21ac0b581f3df995d3ba3160cb053d1a6cf174af
lizhen <nehzilrz@gmail.com>
2018-09-13 12:49:33 +0800

    update

--- config.go
+++ config.go
@@ -2,19 +2,20 @@ package torrentfs
 
 // Config ...
 type Config struct {
-	DataDir string
-	RpcURI  string
-	IpcPath string
+	DataDir string `toml:",omitempty"`
+	RpcURI  string `toml:",omitempty"`
+	IpcPath string `toml:",omitempty"`
 	// Host is the host interface on which to start the storage server. If this
 	// field is empty, no storage will be started.
-	Host    string
+	Host    string `toml:",omitempty"`
 	// Port is the TCP port number on which to start the storage server. The
 	// default zero value is/ valid and will pick a port number randomly.
-	Port    int
-	DefaultTrackers []string
+	Port    int `toml:",omitempty"`
+	DefaultTrackers []string `toml:",omitempty"`
 }
-
+// DefaultConfig contains default settings for the storage.
 var DefaultConfig = Config{
 	Host:    "localhost",
-	Port:    8080,
+	Port:    8085,
+	DefaultTrackers: []string{"http://47.52.39.170:5008/announce"},
 }
