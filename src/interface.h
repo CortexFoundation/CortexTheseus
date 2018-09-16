18935f1878c78348fb737a052e35537eb9cd1e36
杨培文 (Yang Peiwen) <915505626@qq.com>
2018-09-16 13:40:46 +0800

    Update interface.h

--- src/interface.h
+++ src/interface.h
@@ -3,5 +3,5 @@
 void *load_model(char *cfg_fname, char *model_bin_fname);
 void free_model(void *net);
 int get_output_length(void *net);
-int predict(void * model, char * image_data, char **output_data);
+int predict(void *model, char *image_data, char *output_data);
 #endif
