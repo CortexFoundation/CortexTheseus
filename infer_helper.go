8d79da4676864541e085775aa585d79385fb5948
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-16 17:40:57 +0800

    modify interface

--- infer_helper.go
+++ infer_helper.go
@@ -2,50 +2,63 @@ package main
 
 /*
 #cgo LDFLAGS: -L./ -lcortexnet -lstdc++
-#cgo CFLAGS: -I./int_mnist_model 
+#cgo CFLAGS: -I./int_mnist_model
 
 #include "interface.h"
- */
+*/
 import "C"
 import (
+	"errors"
 	"fmt"
 	"unsafe"
 )
 
-func Infer(modelDir, inputDir string) string {
+func infer_core(modelDir, inputDir string) (uint64, error) {
 	fmt.Println(modelDir, inputDir)
 	net := C.load_model(
-		C.CString(modelDir + "/data/params"),
-		C.CString(modelDir + "/data/symbol"))
+		C.CString(modelDir+"/data/params"),
+		C.CString(modelDir+"/data/symbol"))
 
-	pred := C.predict(
-		net,
-		C.CString(inputDir + "/data"))
-
-	tmp := (*[10]byte)(unsafe.Pointer(pred))
-	fmt.Println(tmp)
-	fmt.Print("Result: ")
-	for idx := 0; idx < 10; idx++ {
-		fmt.Printf("%d ", tmp[idx])
+	resLen := C.getOutputLength(net)
+	if resLen == 0 {
+		return 0, errors.New("Model result len is 0")
 	}
-	fmt.Println("")
 
-	res := C.GoBytes(unsafe.Pointer(pred), 10)
+	pred := make([]byte, resLen)
 
-	fmt.Print("Result: ")
-	for idx := 0; idx < len(res); idx++ {
+	flag := C.predict(
+		net,
+		C.CString(inputDir+"/data"),
+		pred)
+
+	res := C.GoBytes(unsafe.Pointer(pred), resLen)
+	max := uint64(res[0])
+	label := 0
+	for idx := 0; idx < resLen; idx++ {
+		if uint64(res[idx]) > maxLabel {
+			maxLabel = uint64(res[idx])
+			label = idx
+		}
 		fmt.Printf("%d ", res[idx])
 	}
-	fmt.Println("")
 
 	C.free_model(net)
 
-	return C.GoString(pred)
+	return label, nil
 }
 
+func Infer(modelDir, inputDir string, resultCh chan uint64, errCh chan error) {
+	label, err := infer_core(modelDir, inputDir)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	resultCh <- label
+}
 
 func main() {
-	Infer("./infer_data/model", "./infer_data/input")
+	infer_core("./infer_data/model", "./infer_data/input")
 	// fmt.Println("Result: " + res)
 	// fmt.Println("Result: " + res + " Length: " + string(len(res)))
 }
