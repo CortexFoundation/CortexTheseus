d3dbc4b52566b49c081f98a0d26c9e90429b9977
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-18 15:01:21 +0800

    Add Infer Cache & File Exists Check

--- infer_test.go
+++ infer_test.go
@@ -6,7 +6,11 @@ import (
 )
 
 func TestLocalInfer(t *testing.T) {
-	label, err := InferCore("./infer_data/model", "./infer_data/num")
+	modelCfg := "./infer_data/model/data/params"
+	modelBin := "./infer_data/model/data/symbol"
+	image := "./infer_data/num_nine/data"
+	label, err := InferCore(modelCfg, modelBin, image)
+	// label, err := InferCore("./infer_data/model", "./infer_data/num")
 
 	fmt.Println(label, err)
 }
