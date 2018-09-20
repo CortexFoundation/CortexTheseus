968c2e27f767b3e465f15327463d7ba6b0df055b
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-20 15:41:51 +0800

    stash

--- torrentfs/config.go
+++ torrentfs/config.go
@@ -7,17 +7,18 @@ type Config struct {
 	IpcPath string `toml:",omitempty"`
 	// Host is the host interface on which to start the storage server. If this
 	// field is empty, no storage will be started.
-	Host    string `toml:",omitempty"`
+	Host string `toml:",omitempty"`
 	// Port is the TCP port number on which to start the storage server. The
 	// default zero value is/ valid and will pick a port number randomly.
-	Port    int `toml:",omitempty"`
+	Port            int    `toml:",omitempty"`
 	DefaultTrackers string `toml:",omitempty"`
-	SyncMode string `toml:",omitempty"`
+	SyncMode        string `toml:",omitempty"`
 }
+
 // DefaultConfig contains default settings for the storage.
 var DefaultConfig = Config{
-	Host:    "localhost",
-	Port:    8085,
+	Host:            "localhost",
+	Port:            8085,
 	DefaultTrackers: "http://47.52.39.170:5008/announce",
-	SyncMode: "full",
+	SyncMode:        "full",
 }
