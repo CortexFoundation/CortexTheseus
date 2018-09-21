9a2e237d44474760900b41fa05d36b4100af1212
ucwong <ucwong@126.com>
2018-09-21 14:28:57 +0800

    gas 1048576

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -20,7 +20,7 @@ import "math/big"
 
 const (
 	GasLimitBoundDivisor uint64 = 1024    // The bound divisor of the gas limit, used in update calculations.
-	MinGasLimit          uint64 = 4096 // Minimum the gas limit may ever be.
+	MinGasLimit          uint64 = 1048576 // Minimum the gas limit may ever be.
 	GenesisGasLimit      uint64 = 16777216 // Gas limit of the Genesis block.
 
 	MaximumExtraDataSize  uint64 = 32    // Maximum size extra data may be after Genesis.
