beea679cac4af7beb9803dad6b55a9b20e1ecf3d
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-22 13:19:33 +0800

    Modify Infer Interface

--- infer_helper.go
+++ infer_helper.go
@@ -2,7 +2,7 @@ package infernet
 
 /*
 #cgo LDFLAGS: -L./ -Wl,-rpath,./build/bin/ -lcortexnet
-#cgo CFLAGS: -I./src
+#cgo CFLAGS: -I./int_mnist_model
 
 #include "interface.h"
 */
@@ -60,13 +60,23 @@ func InferCore(modelCfg, modelBin, image string) (uint64, error) {
 		return 0, errors.New("Predict Error")
 	}
 
-	// Find the maximum possibility of label
 	max := int8(res[0])
 	label := uint64(0)
-	for idx := 1; idx < resLen; idx++ {
-		if int8(res[idx]) > max {
-			max = int8(res[idx])
-			label = uint64(idx)
+
+	// If result length large than 1, find the index of max value;
+	// Else the question is two-classify model, and value of result[0] is the prediction.
+	if resLen > 1 {
+		for idx := 1; idx < resLen; idx++ {
+			if int8(res[idx]) > max {
+				max = int8(res[idx])
+				label = uint64(idx)
+			}
+		}
+	} else {
+		if max > 0 {
+			label = 1
+		} else {
+			label = 0
 		}
 	}
 
