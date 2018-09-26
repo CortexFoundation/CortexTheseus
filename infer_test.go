a75a51716359109fd0f74cda1fa70fa936d41c72
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-26 17:59:26 +0800

    infer test

--- infer_test.go
+++ infer_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestLocalInfer(t *testing.T) {
-	modelCfg := "./infer_data/model/data/params"
-	modelBin := "./infer_data/model/data/symbol"
+	modelCfg := "./infer_data/model/data/symbol"
+	modelBin := "./infer_data/model/data/params"
 	image := "./infer_data/num_nine/data"
 	label, err := InferCore(modelCfg, modelBin, image)
 	// label, err := InferCore("./infer_data/model", "./infer_data/num")
