ed2e96cdbcb7654ad9a2d347d05ee1d32f51840e
Zhen Li <41105464+lizhencortex@users.noreply.github.com>
2018-09-26 20:15:32 +0800

    'update'

--- node/defaults.go
+++ node/defaults.go
@@ -58,11 +58,11 @@ func DefaultDataDir() string {
 	home := homeDir()
 	if home != "" {
 		if runtime.GOOS == "darwin" {
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "Cortex")
 		} else if runtime.GOOS == "windows" {
-			return filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			return filepath.Join(home, "AppData", "Roaming", "Cortex")
 		} else {
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".cortex")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
