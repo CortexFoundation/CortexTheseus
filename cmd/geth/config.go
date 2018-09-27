87bb49e4419dc4b9de8a30bb5fe0da8c25e98d9a
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-09-27 13:07:27 +0800

    stash

--- cmd/geth/config.go
+++ cmd/geth/config.go
@@ -103,9 +103,9 @@ func defaultNodeConfig() node.Config {
 	cfg := node.DefaultConfig
 	cfg.Name = clientIdentifier
 	cfg.Version = params.VersionWithCommit(gitCommit)
-	cfg.HTTPModules = append(cfg.HTTPModules, "eth", "shh")
-	cfg.WSModules = append(cfg.WSModules, "eth", "shh")
-	cfg.IPCPath = "geth.ipc"
+	cfg.HTTPModules = append(cfg.HTTPModules, "ctx", "shh")
+	cfg.WSModules = append(cfg.WSModules, "ctx", "shh")
+	cfg.IPCPath = "cgb.ipc"
 	return cfg
 }
 
