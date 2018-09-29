de5c7ad9ef93bcfd647670bdb02cf3f524d77325
flametbs@gmail.com <flametbs@gmail.com>
2018-09-29 17:47:10 +0800

    nominer

--- cmd/miner/main.go
+++ cmd/miner/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/consensus/cuckoo"
 	"github.com/ethereum/go-ethereum/core/types"
 	cuckoo_gpu "github.com/ethereum/go-ethereum/miner/cuckoocuda"
-	_ "log"
+	"log"
 	"math/rand"
 	"net"
 	"os"
@@ -53,7 +53,7 @@ type ReqObj struct {
 
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		log.Println("%s", err.Error())
 		os.Exit(1)
 	}
 }
@@ -63,7 +63,7 @@ func (cm* Cortex)read() map[string]interface{} {
 	for {
 		tmp, isPrefix, err := cm.reader.ReadLine()
 		if err == io.EOF {
-			fmt.Println("Tcp disconnectted")
+			log.Println("Tcp disconnectted")
 			cm.conn.Close()
 			cm.conn = nil
 			cm.consta.lock.Lock()
@@ -94,7 +94,7 @@ func (cm* Cortex)write(reqObj ReqObj) {
 
 //	init cortex miner
 func (cm* Cortex)init() (*net.TCPConn){
-	fmt.Println("init")
+	log.Println("Cortex Init")
 	//cm.server = "cortex.waterhole.xyz:8008"
 	//cm.server = "localhost:8009"
 	//cm.account = "0xc3d7a1ef810983847510542edfd5bc5551a6321c"
@@ -210,14 +210,14 @@ func (cm* Cortex) miningOnce() {
 					status, sols := cuckoo.CuckooFindSolutions(header, curNonce)
 					if status != 0 {
 						if verboseLevel >= 3 {
-							fmt.Println("result: ", status, sols)
+							log.Println("result: ", status, sols)
 						}
 						for _, solUint32 := range sols {
 							var sol types.BlockSolution
 							copy(sol[:], solUint32)
 							sha3hash := common.BytesToHash(cuckoo.Sha3Solution(&sol))
 							if verboseLevel >= 3 {
-								fmt.Println(curNonce, "\n sol hash: ", hex.EncodeToString(sha3hash.Bytes()), "\n tgt hash: ", hex.EncodeToString(tgtDiff.Bytes()))
+								log.Println("Nonce: ", curNonce, "\n Sol hash: ", hex.EncodeToString(sha3hash.Bytes()), "\n Tgt hash: ", hex.EncodeToString(tgtDiff.Bytes()))
 							}
 							if sha3hash.Big().Cmp(tgtDiff.Big()) <= 0 {
 								result = sol
@@ -225,7 +225,7 @@ func (cm* Cortex) miningOnce() {
 								digest := common.Uint32ArrayToHexString([]uint32(result[:]))
 								ok, _ := cuckoo.CuckooVerifyHeader(header[:], curNonce, &sol)
 								if !ok {
-									fmt.Println("verify failed", header[:], curNonce, &sol)
+									log.Println("verify failed", header[:], curNonce, &sol)
 								} else {
 									solChan <- Task{Nonce: nonceStr, Header: taskHeader, Solution: digest}
 								}
@@ -236,14 +236,14 @@ func (cm* Cortex) miningOnce() {
 						status, sols := cuckoo_gpu.CuckooFindSolutionsCuda(header, curNonce)
 						if status != 0 {
 							if verboseLevel >= 3 {
-								fmt.Println("result: ", status, sols)
+								log.Println("result: ", status, sols)
 							}
 							for _, solUint32 := range sols {
 								var sol types.BlockSolution
 								copy(sol[:], solUint32)
 								sha3hash := common.BytesToHash(cuckoo.Sha3Solution(&sol))
 								if verboseLevel >= 3 {
-									fmt.Println(curNonce, "\n sol hash: ", hex.EncodeToString(sha3hash.Bytes()), "\n tgt hash: ", hex.EncodeToString(tgtDiff.Bytes()))
+									log.Println(curNonce, "\n sol hash: ", hex.EncodeToString(sha3hash.Bytes()), "\n tgt hash: ", hex.EncodeToString(tgtDiff.Bytes()))
 								}
 								if sha3hash.Big().Cmp(tgtDiff.Big()) <= 0 {
 									result = sol
@@ -251,7 +251,7 @@ func (cm* Cortex) miningOnce() {
 									digest := common.Uint32ArrayToHexString([]uint32(result[:]))
 									ok, _ := cuckoo.CuckooVerifyHeader(header[:], curNonce, &sol)
 									if !ok {
-										fmt.Println("verify failed", header[:], curNonce, &sol)
+										log.Println("verify failed", header[:], curNonce, &sol)
 									} else {
 										solChan <- Task{Nonce: nonceStr, Header: taskHeader, Solution: digest}
 									}
@@ -270,13 +270,15 @@ func (cm* Cortex) miningOnce() {
 			if cm.consta.state == false {
 				 return
 			}
-			fmt.Println("Received: ", msg)
+			if (cm.verboseLevel >= 4) {
+				log.Println("Received: ", msg)
+			}
 			reqId, _ := msg["id"].(float64)
 			if uint32(reqId) == 100 || uint32(reqId) == 0 {
 				workInfo, _ := msg["result"].([]interface{})
 				if len(workInfo) >= 3 {
 					taskHeader, taskNonce, taskDifficulty = workInfo[0].(string), workInfo[1].(string), workInfo[2].(string)
-					fmt.Println("Get Work: ", taskHeader, taskNonce, taskDifficulty)
+					log.Println("Get Work: ", taskHeader, taskDifficulty)
 					currentTask_.Lock.Lock()
 					currentTask_.TaskQ.Nonce = taskNonce
 					currentTask_.TaskQ.Header = taskHeader
