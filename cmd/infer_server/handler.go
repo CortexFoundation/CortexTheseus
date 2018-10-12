959c3dfe413c06d8b8b961d74142ef1ab26b284c
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-10-12 16:56:54 +0800

    stash

--- cmd/infer_server/handler.go
+++ cmd/infer_server/handler.go
@@ -50,13 +50,13 @@ func inputContentHandler(w http.ResponseWriter, inferWork *InferWork) {
 	}
 
 	log.Debug("JSON-RPC request | ctx_getSolidityBytes", "address", addr, "slot", slot, "block number", "latest")
-	var result hexutil.Bytes
-	if rpcErr := rpcClient.CallContext(context.Background(), &result, "ctx_getSolidityBytes", addr, slot, "latest"); rpcErr != nil {
+	var inputArray hexutil.Bytes
+	if rpcErr := rpcClient.CallContext(context.Background(), &inputArray, "ctx_getSolidityBytes", addr, slot, "latest"); rpcErr != nil {
 		log.Warn("JSON-RPC request failed", "error", rpcErr)
 		RespErrorText(w, "JSON-RPC invoke ctx_getSolidityBytes", "error", rpcErr, "address", addr, "slot", slot)
 		return
 	}
-	var inputArray = result[:]
+
 	log.Debug("Infer Task By Input Content", "model info hash", inferWork.ModelHash, "input content", inputArray)
 	label, err := infer.Engine().InferByInputContent(inferWork.ModelHash, inputArray)
 
