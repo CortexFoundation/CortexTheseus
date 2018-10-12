d67357229fdc55dcb7ffa43d590bc47ed14516df
flametbs@gmail.com <flametbs@gmail.com>
2018-10-12 21:12:47 +0800

    mnist dapp toolset

---
+++ example/fillMnist.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+data = np.load('/home/tian/data/ctxc-42-11/warehouse/' + sys.argv[1] + '/data')
+print >> sys.stderr, data
+for i in range(28):
+    for j in range(28):
+        v = int(data[0][i][j])
+        print ("%064x%064x%064x" % (i,j ,v))
+        print >>sys.stderr, "%02x" % v,
