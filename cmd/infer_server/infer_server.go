8cb9b9831821efc87539228d78058389d3883595
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-10-12 14:03:52 +0800

    Adjust infer server response logic

--- cmd/infer_server/infer_server.go
+++ cmd/infer_server/infer_server.go
@@ -42,22 +42,22 @@ type InferWork struct {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		fmt.Fprintf(w, `{"msg": "error", "info": "Request method not POST"}`)
+		RespErrorText(w, ErrRequestMethodNotPost)
 		return
 	}
 
 	body, rerr := ioutil.ReadAll(r.Body)
 	if rerr != nil {
-		fmt.Fprintf(w, `{"msg": "error", "info": "Read request body error"}`)
+		RespErrorText(w, ErrRequestBodyRead)
 		return
 	}
 
-	log.Debug("Handler Info", "request", r, "body", string(body))
+	log.Trace("Handler Info", "request", r, "body", string(body))
 
 	var inferWork InferWork
 
 	if err := json.Unmarshal(body, &inferWork); err != nil {
-		fmt.Fprintf(w, `{"msg": "error", "info": "Data parse error"}`)
+		RespErrorText(w, ErrDataParse)
 		return
 	}
 
