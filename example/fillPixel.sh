aab1cec5a824a5f032e90f2be9adc21db3553179
flametbs@gmail.com <flametbs@gmail.com>
2018-10-16 15:25:32 +0800

    remote infer with inferarry; fix bug: infer server cache

--- example/fillPixel.sh
+++ example/fillPixel.sh
@@ -1,3 +1,3 @@
 ARGS=$1
-CONTRACT=0x25befe823553cd37d93d91a44cf5f5c0ba568e99
-curl -X POST http://localhost:30089 -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0x432fee7c11Afc2F67C37b079aC284fc42Adee3bB", "to": "'$CONTRACT'", "input": "0x7468847d'$ARGS'", "price":"0x1", "gas":"0x3a1200"}], "id":"0x1"}'
+CONTRACT=0xf3d04c84ee4a2d1a9417ed71a8f511d546963e0c
+curl -X POST http://localhost:30089 -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from": "0xa96120378fb2c0bab24489902c7053351e023fc6", "to": "'$CONTRACT'", "input": "0x7468847d'$ARGS'", "price":"0x1", "gas":"0x1d1200"}], "id":"0x1"}'
