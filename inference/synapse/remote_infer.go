918881f5604c717db95d5bd190803b6fd09907af
Longtao Wang <longtao.wang@cortexlabs.ai>
2018-10-25 19:48:40 +0800

    Add binray only package script
    Retab code
    
    consensus error while error caused by infer built-in torrent fs

--- inference/synapse/remote_infer.go
+++ inference/synapse/remote_infer.go
@@ -12,21 +12,22 @@ import (
 
 func (s *Synapse) RemoteInferByInfoHash(modelInfoHash, inputInfoHash, uri string) (uint64, error) {
 	requestBody := fmt.Sprintf(`{"Type": 1, "ModelHash":"%s", "InputHash":"%s"}`, modelInfoHash, inputInfoHash)
-	log.Trace(fmt.Sprintf("%v", requestBody))
+	log.Debug("Remote Inference", "request", requestBody)
 
 	return s.sendRequest(requestBody, uri)
 }
 
 func (s *Synapse) RemoteInferByInputContent(modelInfoHash, uri string, addr, slot, blockNumber string) (uint64, error) {
 	requestBody := fmt.Sprintf(`{"Type": 2, "ModelHash":"%s", "InputAddress":"%s", "InputSlot":"%s", "InputBlockNumber":"%s"}`, modelInfoHash, addr, slot, blockNumber)
-	log.Trace(fmt.Sprintf("%v", requestBody))
+	log.Debug("Remote Inference", "request", requestBody)
 
 	return s.sendRequest(requestBody, uri)
 }
+
 func (s *Synapse) sendRequest(requestBody, uri string) (uint64, error) {
 	cacheKey := RLPHashString(requestBody)
 	if v, ok := s.simpleCache.Load(cacheKey); ok && !s.config.IsNotCache {
-		log.Debug("Infer Success via Cache", "result", v.(uint64))
+		log.Debug("Infer Succeed via Cache", "result", v.(uint64))
 		return v.(uint64), nil
 	}
 
@@ -38,30 +39,30 @@ func (s *Synapse) sendRequest(requestBody, uri string) (uint64, error) {
 		return 0, errors.New(fmt.Sprintf("%s | %s | %s | %s | %v", "evm.Infer: External Call Error: ", requestBody, resp, uri, err))
 	}
 
-	log.Debug("Remote Infer", "Response", resp.String())
+	log.Debug("Remote Inference", "response", resp.String())
 
 	js, js_err := simplejson.NewJson([]byte(resp.String()))
 	if js_err != nil {
-		return 0, errors.New(fmt.Sprintf("evm.Infer: External Call Error | %v ", js_err))
+		return 0, errors.New(fmt.Sprintf("Remote Infer: resonse json parse error | %v ", js_err))
 	}
 
 	msg, msgErr := js.Get("msg").String()
 	if msgErr != nil {
-		return 0, errors.New(fmt.Sprintf("evm.Infer: External Call Error | %v ", msgErr))
+		return 0, errors.New(fmt.Sprintf("Remote Infer: response `msg` parse error | %v ", msgErr))
 	}
 
 	int_output_tmp, out_err := js.Get("info").String()
 	if out_err != nil {
-		return 0, errors.New(fmt.Sprintf("evm.Infer: External Call Error | %v ", out_err))
+		return 0, errors.New(fmt.Sprintf("Remote Infer: response `info` parse error | %v ", out_err))
 	}
 
 	if msg != "ok" {
-		return 0, errors.New(fmt.Sprintf("evm.Infer: External Response not OK | %v ", int_output_tmp))
+		return 0, errors.New(int_output_tmp)
 	}
 
 	uint64_output, err := strconv.ParseUint(int_output_tmp, 10, 64)
 	if err != nil {
-		return 0, errors.New("evm.Infer: Type Conversion Error")
+		return 0, errors.New("Remote Infer: result conversion error")
 	}
 
 	if !s.config.IsNotCache {
