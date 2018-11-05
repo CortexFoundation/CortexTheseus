f67a5357224a6e94b19b3917a5ad212abb17174c
zhangkaihuo <zhangkaihuo@cortexlabs.ai>
2018-11-05 19:12:55 +0800

    update device id

--- miner/cuckoocuda/src/ocl/include/device.h
+++ miner/cuckoocuda/src/ocl/include/device.h
@@ -12,12 +12,12 @@ inline cl_device_id getFirstDevice(cl_platform_id platformId){
 inline cl_device_id getOneDevice(cl_platform_id platformId, unsigned int deviceId){
 	cl_uint numDevices = 0;
 	cl_int err = clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU, 0, NULL, &numDevices);
-	if(err != CL_SUCCESS || numDevices < deviceId){
+	if(err != CL_SUCCESS || numDevices <= deviceId){
 		perror("query device failed.\n");
 		return NULL;
 	}
 	cl_device_id *deviceIds = (cl_device_id*)malloc(sizeof(cl_device_id) * numDevices);
 	err = clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU, numDevices, deviceIds, NULL);
-	return deviceIds[deviceId-1];
+	return deviceIds[deviceId];
 }
 #endif
