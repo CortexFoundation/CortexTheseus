e0dec95f4e3b6b5478f731651888f3490228ce2a
zhangkaihuo <zhangkaihuo@cortexlabs.ai>
2018-11-06 11:11:51 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-make clean -C miner/cuckoocuda/
-make -C miner/cuckoocuda/
+make clean -C miner/libcuckoo/
+make -C miner/libcuckoo/
 make clean && make
-cp miner/cuckoocuda/src/wlt_trimmer.cl ./
-./build/bin/miner --account "0xe291d43ad2eb6ea04e9f5e1a0c67f970702f8bd6" --deviceid 0 --devicetype gpu --pool_uri 192.168.50.75:8018 --verbosity 5
+cp miner/libcuckoo/src/opencl/wlt_trimmer.cl ./
+./build/bin/opencl_miner --account "0xe291d43ad2eb6ea04e9f5e1a0c67f970702f8bd6" --deviceid 0 --devicetype gpu --pool_uri 192.168.50.75:8018 --verbosity 5
 
