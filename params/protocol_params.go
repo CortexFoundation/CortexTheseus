8b2616f9c289a9cb8d0f29446594fc724047e845
ucwong <ucwong@126.com>
2019-04-19 15:44:10 +0800

    ceil

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -20,10 +20,10 @@ import "math/big"
 
 const (
 	GasLimitBoundDivisor uint64 = 1024     // The bound divisor of the gas limit, used in update calculations.
-	MinGasLimit          uint64 = 24000000 // Minimum the gas limit may ever be.
-	GenesisGasLimit      uint64 = 24000000 // Gas limit of the Genesis block.
-	MinerGasFloor        uint64 = 24000000
-	MinerGasCeil         uint64 = 80000000
+	MinGasLimit          uint64 = 100000000 // Minimum the gas limit may ever be.
+	GenesisGasLimit      uint64 = 100000000 // Gas limit of the Genesis block.
+	MinerGasFloor        uint64 = 100000000
+	MinerGasCeil         uint64 = 100000000
 
 	MaximumExtraDataSize  uint64 = 32    // Maximum size extra data may be after Genesis.
 	ExpByteGas            uint64 = 10    // Times ceil(log256(exponent)) for the EXP instruction.
