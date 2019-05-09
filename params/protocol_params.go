9a60dccfa5ae94e86aef65ae62eed3b24eca9f32
ucwong <ucwong@126.com>
2019-05-09 11:59:08 +0800

    safe diff

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -93,7 +93,7 @@ const (
 
 var (
 	DifficultyBoundDivisor = big.NewInt(2) // The bound divisor of the difficulty, used in the update calculations.
-	GenesisDifficulty      = big.NewInt(64) // Difficulty of the Genesis block.
+	GenesisDifficulty      = big.NewInt(512) // Difficulty of the Genesis block.
 	MinimumDifficulty      = big.NewInt(2) // The minimum that the difficulty may ever be.
 
 	MeanDifficultyBoundDivisor = big.NewInt(1024)
