87baca190547e474bff77b47da403240c27f160f
ucwong <ucwong@126.com>
2019-05-28 13:37:38 +0800

    ai invoke base cost 100000 gas

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -39,7 +39,7 @@ const (
 	SstoreSetGas          uint64 = 20000   // Once per SLOAD operation.
 	LogDataGas            uint64 = 8       // Per byte in a LOG* operation's data.
 	CallStipend           uint64 = 2300    // Free gas given at beginning of call.
-	CallInferGas          uint64 = 1000000 // Base gas for call infer
+	CallInferGas          uint64 = 100000 // Base gas for call infer
 
 	Sha3Gas         uint64 = 30    // Once per SHA3 operation.
 	Sha3WordGas     uint64 = 6     // Once per word of the SHA3 operation's data.
