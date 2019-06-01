0a48a2a1783c936e27737e03e918774898a2759c
zhangkaihuo <zhangkaihuo@cortexlabs.ai>
2019-06-01 20:06:39 +0800

    fix iou overflow int64

--- tests/test_op.py
+++ tests/test_op.py
@@ -2,9 +2,9 @@ import mxnet as mx
 from mxnet import ndarray as nd
 import numpy as np
 
-a = nd.arange(0, stop=20*16*196*3).reshape((20, 16, 196, 3))
+a = nd.arange(0, stop=60).reshape((5, 2, 2, 3))
 #c = nd.transpose(a, axes=(0, 2, 3, 1))
-c = nd.expand_dims(a, axes=(-1))
+c = nd.expand_dims(a, axis=(-1))
 print(c.shape)
 print(c.asnumpy().flatten())
 #nd.slice()
@@ -12,7 +12,7 @@ print(c.asnumpy().flatten())
 import json
 js = json.load(open('/home/kaihuo/model_storage/yolo3_darknet53/data/symbol'))
 for idx, x in enumerate(js['nodes']):
-    if (x['name'] == 'expand_dims'):
+    if (x['name'] == 'strided_slice'):
         for input_idx in x['inputs']:
             child_id = input_idx[0]
             print (input_idx, child_id, js['attrs']['shape'][1][child_id])
