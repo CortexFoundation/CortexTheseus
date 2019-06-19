42002016d2088d0413daae834b600facdf25b00d
lizhen <zhen@cortexlabs.ai>
2019-06-19 15:05:29 +0800

    fix mergre conflict

--- params/config.go
+++ params/config.go
@@ -59,7 +59,7 @@ var (
 	}
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:        big.NewInt(25),
+		ChainID:        big.NewInt(21),
 		HomesteadBlock: big.NewInt(0),
 		DAOForkBlock:   big.NewInt(0),
 		DAOForkSupport: false,
