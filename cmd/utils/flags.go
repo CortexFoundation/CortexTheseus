04bffc76a249521916e50ed1aa9667f1e28bd2b4
zhen <zhen@cortexlabs.ai>
2019-06-23 18:55:18 +0800

    update

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -208,7 +208,7 @@ var (
 	StorageTrackerFlag = cli.StringFlag{
 		Name:  "storage.tracker",
 		Usage: "P2P storage tracker list",
-		Value: "",
+		Value: strings.Join(torrentfs.DefaultConfig.DefaultTrackers, ","),
 	}
 	// Dashboard settings
 	// DashboardEnabledFlag = cli.BoolFlag{
