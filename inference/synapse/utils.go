f625a009b34955d86ca9042c9de07eb2c46299b2
Longtao Wang <longtao.wang@cortexlabs.ai>
2019-07-11 16:14:06 +0800

    Fixed bugs

--- inference/synapse/utils.go
+++ inference/synapse/utils.go
@@ -1,15 +1,15 @@
 package synapse
 
 import (
-	"fmt"
+	"encoding/binary"
 	"errors"
+	"fmt"
 	"github.com/CortexFoundation/CortexTheseus/common"
 	"github.com/CortexFoundation/CortexTheseus/common/hexutil"
 	"github.com/CortexFoundation/CortexTheseus/crypto/sha3"
 	"github.com/CortexFoundation/CortexTheseus/inference"
-	"github.com/CortexFoundation/CortexTheseus/rlp"
 	"github.com/CortexFoundation/CortexTheseus/log"
-	"encoding/binary"
+	"github.com/CortexFoundation/CortexTheseus/rlp"
 )
 
 func ReadData(r *inference.NpyReader) ([]byte, error) {
@@ -18,11 +18,11 @@ func ReadData(r *inference.NpyReader) ([]byte, error) {
 		derr error
 	)
 	log.Debug("ReadData", "r.Dtype", r.Dtype)
-	if (r.Dtype == "i1") {
+	if r.Dtype == "i1" {
 		data, derr = r.GetBytes()
-	} else if (r.Dtype == "i4") {
+	} else if r.Dtype == "i4" {
 		i4_data, i4_derr := r.GetInt32()
-		data = make([]byte, len(i4_data) * 4)
+		data = make([]byte, len(i4_data)*4)
 		if i4_derr != nil {
 			return nil, derr
 		}
@@ -31,7 +31,7 @@ func ReadData(r *inference.NpyReader) ([]byte, error) {
 			tmp := make([]byte, 8)
 			binary.PutUvarint(tmp[:], uint64(i4_data[idx]))
 			// fmt.Println(uint64(i4_data[idx]))
-			copy(data[idx * 4: idx*4 + 4], tmp[:4])
+			copy(data[idx*4:idx*4+4], tmp[:4])
 			// fmt.Println(data[idx * 4: idx * 4 + 4], tmp[:4])
 		}
 		// fmt.Println("data = ", data)
@@ -55,7 +55,7 @@ func ReadData(r *inference.NpyReader) ([]byte, error) {
 // 	for i, v := range data {
 // 		data[i] = uint8(v) / 2
 // 	}
-// 
+//
 // 	return nil
 // }
 
@@ -67,13 +67,13 @@ func RLPHashString(x interface{}) string {
 }
 
 func SwitchEndian(data []byte, bytes int) ([]byte, error) {
-	if (len(data) % bytes != 0) {
+	if len(data)%bytes != 0 {
 		return nil, errors.New(fmt.Sprintf("data is not aligned with %d", bytes))
 	}
 	ret := make([]byte, len(data))
 	for i := 0; i < len(data); i += bytes {
 		for j := 0; j < bytes; j++ {
-			ret[i + bytes - j - 1] = data[i + j]
+			ret[i+bytes-j-1] = data[i+j]
 		}
 	}
 	return ret, nil
@@ -81,7 +81,7 @@ func SwitchEndian(data []byte, bytes int) ([]byte, error) {
 
 func ToAlignedData(data []byte, bytes int) ([]byte, error) {
 	data_aligned := make([]byte, len(data))
-	if (bytes > 1) {
+	if bytes > 1 {
 		tmp_res, input_conv_err := SwitchEndian(data, int(bytes))
 		if input_conv_err != nil {
 			return nil, input_conv_err
