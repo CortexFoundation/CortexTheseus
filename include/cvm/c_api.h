44ed5f3027d1598fe052dc1a6d0e2824a9d880dc
Longtao Wang <longtao.wang@cortexlabs.ai>
2019-07-11 12:37:01 +0800

    Update CVM executor interface

--- include/cvm/c_api.h
+++ include/cvm/c_api.h
@@ -19,40 +19,47 @@
 #define CVM_DLL __attribute__((visibility("default")))
 #endif
 
-/*! \brief manually define unsigned int */
-typedef unsigned int nn_uint;
+/*
+ * CVMAPIInterface
+ * parameters : input, output or attributes
+ * returns    : API execute status
+ *            0 stands for succeed;
+ *            -1 stands for logic error;
+ *            -2 stands for runtime error;
+ */
+enum CVMStatus {
+  SUCCEED = 0,
+  ERROR_LOGIC,
+  ERROR_RUNTIME,
+  ERROR_UNKNOWN
+};
 
-/*! \brief handle to a function that takes param and creates symbol */
+typedef void* ModelHandler;
+typedef long long* IntHandler;
+typedef char* StringHandler;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void* CVMAPILoadModel(const char *graph_fname, int  json_len, const char *model_fname, int mdoel_len, int device_type, int device_id);
+CVMStatus CVMAPILoadModel(const char *graph_json, int graph_strlen,
+                          const char *param_bytes, int param_strlen,
+                          ModelHandler *net, // pass reference of network
+                          int device_type, int device_id);
+CVMStatus CVMAPIFreeModel(ModelHandler net);
+CVMStatus CVMAPIInference(ModelHandler net, char *input_data, StringHandler output_data);
 
-void CVMAPIFreeModel(void* model);
+CVMStatus CVMAPIGetVersion(ModelHandler net, StringHandler version);
+CVMStatus CVMAPIGetPreprocessMethod(ModelHandler net, StringHandler method);
 
-int CVMAPIGetInputLength(void* model);
+CVMStatus CVMAPIGetInputLength(ModelHandler net, IntHandler size);
+CVMStatus CVMAPIGetOutputLength(ModelHandler net, IntHandler size);
+CVMStatus CVMAPIGetInputTypeSize(ModelHandler net, IntHandler size);
+CVMStatus CVMAPIGetOutputTypeSize(ModelHandler net, IntHandler size);
 
-int CVMAPIGetOutputLength(void* model);
-
-void CVMAPIGetVersion(void* model, char* version);
-
-void CVMAPIGetPreprocessMethod(void* model, char* method);
-
-int CVMAPIInfer(void* model, char *input_data, char *output_data);
-
-int CVMAPIInferInt32(void* model, char *input_data, char *output_data);
-
-int CVMAPISizeOfOutputType(void *model);
-
-int CVMAPISizeOfInputType(void *model);
-
-long long CVMAPIGetGasFromModel(void *model);
-
-long long CVMAPIGetStorageSize(void *model);
-
-long long CVMAPIGetGasFromGraphFile(char *graph_fname);
+CVMStatus CVMAPIGetStorageSize(ModelHandler net, IntHandler gas);
+CVMStatus CVMAPIGetGasFromModel(ModelHandler net, IntHandler gas);
+CVMStatus CVMAPIGetGasFromGraphFile(const char *graph_json, IntHandler gas);
 
 #ifdef __cplusplus
 } /* end extern "C" */
--- tests/test_model.cc
+++ tests/test_model.cc
@@ -13,6 +13,12 @@ using cvm::runtime::Registry;
 
 int use_gpu = 0;
 
+#define CHECK_STATUS(x, msg) \
+  if (x != SUCCEED) { \
+    cerr << "STATUS ERROR: " << x << " " << msg << "\n"; \
+    return -1; \
+  }
+
 void read_data(const char *filename, vector<unsigned long> &shape, vector<int32_t>& data){
     FILE *fp = fopen(filename, "r");
     if(fp == NULL){
@@ -25,7 +31,7 @@ void read_data(const char *filename, vector<unsigned long> &shape, vector<int32_
     uint64_t size = 1;
     for(int i = 0; i < shape_dim; i++){
         int64_t value = 0;
-        fscanf(fp, "%d ", &value);
+        fscanf(fp, "%ld ", &value);
         shape[i] = value;
         size *= shape[i];
     }
@@ -43,24 +49,6 @@ struct OpArgs {
   std::vector<int> arg_tcodes;
   std::vector<int64_t> shape_data;
 };
-
-void test_op_take() {
-  CVMValue t_attr;
-  const PackedFunc* op = Registry::Get("cvm.runtime.cvm.take");
-  cvm::NodeAttrs* attr;
-  std::shared_ptr<OpArgs> arg_ptr = std::make_shared<OpArgs>();
-  t_attr.v_handle = (void*)attr;
-  arg_ptr->arg_values.push_back(t_attr);
-  arg_ptr->arg_tcodes.push_back(kHandle);
-  cvm::runtime::CVMRetValue rv;
-  cvm::runtime::CVMArgs targs(
-      arg_ptr->arg_values.data(),
-      arg_ptr->arg_tcodes.data(),
-      static_cast<int>(arg_ptr->arg_values.size())
-      );
-  // (*op)(ta);
-
-}
 
 int run_LIF(string model_root, int device_type = 0) {
   cvm::runtime::transpose_int8_avx256_transpose_cnt = 0;
@@ -93,19 +81,23 @@ int run_LIF(string model_root, int device_type = 0) {
     params  = string((std::istreambuf_iterator<char>(input_stream)), std::istreambuf_iterator<char>());
     input_stream.close();
   }
-  cvm::runtime::CVMModel* model = static_cast<cvm::runtime::CVMModel*>(
-      CVMAPILoadModel(json.c_str(), json.size(), params.c_str(), params.size(), device_type, 0)
-    );
+  ModelHandler net;
+  auto status = CVMAPILoadModel(json.c_str(), json.size(),
+                                params.c_str(), params.size(),
+                                &net,
+                                device_type, 0);
   cerr << "model loaded\n";
-  if (model == nullptr) {
-    std::cerr << "model loaded failed\n";
-    return -1;
-  }
-  cerr << "ops " << CVMAPIGetGasFromModel(model) / 1024 / 1024 << "\n";
+  CHECK_STATUS(status, "model loaded failed");
+
+  long long gas = 0;
+  status = CVMAPIGetGasFromModel(net, static_cast<IntHandler>(&gas));
+  CHECK_STATUS(status, "gas invalid");
+  cerr << "ops " << gas / 1024 / 1024 << "\n";
   // API only accepts byte array
   vector<char> input, output;
-  int input_size = CVMAPIGetInputLength(model);
-  int output_size = CVMAPIGetOutputLength(model);
+  long long input_size, output_size;
+  CVMAPIGetInputLength(net, &input_size);
+  CVMAPIGetOutputLength(net, &output_size);
   input.resize(input_size, 0); // 1 * 1 * 28 * 28);
   output.resize(output_size, 0); //1 * 10);
   if (model_root.find("trec") != string::npos)
@@ -156,9 +148,11 @@ int run_LIF(string model_root, int device_type = 0) {
   for (int i = 0; i < n_run; i++) {
     if (i % 10 == 0)
       cerr << "i = " << i << "\n";
-    CVMAPIInfer(model, input.data(), output.data());
+    status = CVMAPIInference(net, input.data(), output.data());
+    CHECK_STATUS(status, "inference failed");
   }
-  CVMAPIFreeModel(model);
+  status = CVMAPIFreeModel(net);
+  CHECK_STATUS(status, "free model failed");
   double ellapsed_time = (omp_get_wtime() - start) / n_run;
   cout << "total time : " << ellapsed_time / n_run << "\n";
   cout << "total gemm.trans time: " << cvm::runtime::transpose_int8_avx256_transpose_cnt / n_run << "\n";
