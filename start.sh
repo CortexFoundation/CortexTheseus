0ea66bdf8c1b7c2bba576d99e1684eee8148ae23
ucwong <ucwong@126.com>
2019-07-17 17:24:36 +0800

    fix ps grep

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 ./cvm.sh > /dev/null 2>&1 &
 ./node.sh &
 while true; do
-        server=`ps aux | grep cvm | grep -v grep`
+        server=`ps aux | grep 'cortex cvm' | grep -v grep`
         if [ ! "$server" ]; then
             ./cvm.sh > /dev/null 2>&1 &
             sleep 10
