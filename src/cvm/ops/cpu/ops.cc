3d9bda5f6993cd9105a26d862232197c28e86f7d
Longtao Wang <longtao.wang@cortexlabs.ai>
2019-07-19 15:07:10 +0800

    rebuild broadcast op

--- src/cvm/ops/cpu/ops.cc
+++ src/cvm/ops/cpu/ops.cc
@@ -17,7 +17,6 @@ double cvm_op_cvm_shift_cnt = 0;
 double cvm_op_clip_cnt = 0;
 double cvm_op_dense_cnt = 0;
 double cvm_op_maxpool_cnt = 0;
-double cvm_op_broadcast_cnt = 0;
 double cvm_op_concat_cnt = 0;
 double cvm_op_upsampling_cnt = 0;
 double cvm_op_inline_matmul_cnt = 0;
@@ -566,115 +565,6 @@ CVM_REGISTER_GLOBAL("cvm.runtime.cvm.conv2d")
   print_to_file(y, "conv2d.txt");
 });
 
-inline int32_t broadcast_i_index(int64_t* oshape, uint64_t o_index, int64_t* ishape, int idim, int odim){
-    if(idim == 1 && ishape[0] == 1) return 0;
-    uint64_t index = 0;
-    uint64_t allIndex = 1;
-    for(int i = 0; i < idim; i++){
-        int idx = idim - 1 - i;
-        int ovar = o_index % oshape[idx+odim-idim];
-        if(ovar < ishape[idx]){
-            index += allIndex * ovar;
-        }
-        allIndex =  allIndex * ishape[idx];
-        o_index /= oshape[idx + odim-idim];
-    }
-    return index;
-}
-
-CVM_REGISTER_GLOBAL("cvm.runtime.cvm.broadcast_add")
-    .set_body([](CVMArgs args, CVMRetValue *ret){
-#ifdef CVM_PROFILING
-        double start = omp_get_wtime();
-#endif
-        DLTensor *args0 = args[0];
-        DLTensor *args1 = args[1];
-        DLTensor *args2 = args[2];
-        int32_t *a = static_cast<int32_t*>(args0->data);
-        int32_t *b = static_cast<int32_t*>(args1->data);
-        int32_t *c = static_cast<int32_t*>(args2->data);
-
-        if(getSize(args1) == 1){
-            for(uint64_t i = 0; i < getSize(args2); ++i){
-                c[i] = a[i] + b[0];
-            }
-        }else{
-#pragma omp parallel for
-            for(uint64_t i = 0; i < getSize(args2); ++i){
-                uint64_t o_index = i;//broadcast_o_index(args2->shape, args2->ndim, o_index);
-                int64_t a_index = broadcast_i_index(args2->shape, o_index, args0->shape, args0->ndim, args2->ndim);
-                int64_t b_index = broadcast_i_index(args2->shape, o_index, args1->shape, args1->ndim, args2->ndim);
-                c[i] = a[a_index] + b[b_index];
-            }
-        }
-#ifdef CVM_PROFILING
-        cvm_op_broadcast_cnt += omp_get_wtime() - start;
-#endif
-        print_to_file(args2, "broadcast_add.txt");
-    });
-
-CVM_REGISTER_GLOBAL("cvm.runtime.cvm.broadcast_sub")
-    .set_body([](CVMArgs args, CVMRetValue *ret)
-{
-#ifdef CVM_PROFILING
-        double start = omp_get_wtime();
-#endif
-        DLTensor *args0 = args[0];
-        DLTensor *args1 = args[1];
-        DLTensor *args2 = args[2];
-        int32_t *a = static_cast<int32_t*>(args0->data);
-        int32_t *b = static_cast<int32_t*>(args1->data);
-        int32_t *c = static_cast<int32_t*>(args2->data);
-        if(getSize(args1) == 1){
-            for(uint64_t i = 0; i < getSize(args2); ++i){
-                c[i] = a[i] - b[0];
-            }
-        }else{
-#pragma omp parallel for
-            for(uint64_t i = 0; i < getSize(args2); ++i){
-                uint64_t o_index = i;//broadcast_o_index(args2->shape, args2->ndim, o_index);
-                uint64_t a_index = broadcast_i_index(args2->shape, o_index, args0->shape, args0->ndim, args2->ndim);
-                uint64_t b_index = broadcast_i_index(args2->shape, o_index, args1->shape, args1->ndim, args2->ndim);
-                c[i] = a[a_index] - b[b_index];
-            }
-        }
-#ifdef CVM_PROFILING
-        cvm_op_broadcast_cnt += omp_get_wtime() - start;
-#endif
-        print_to_file(args2, "broadcast_sub.txt");
-});
-
-CVM_REGISTER_GLOBAL("cvm.runtime.cvm.broadcast_mul")
-    .set_body([](CVMArgs args, CVMRetValue *ret){
-#ifdef CVM_PROFILING
-        double start = omp_get_wtime();
-#endif
-        DLTensor *args0 = args[0];
-        DLTensor *args1 = args[1];
-        DLTensor *args2 = args[2];
-        int32_t *a = static_cast<int32_t*>(args0->data);
-        int32_t *b = static_cast<int32_t*>(args1->data);
-        int32_t *c = static_cast<int32_t*>(args2->data);
-        if(getSize(args1) == 1){
-#pragma omp parallel for
-            for(uint64_t i = 0; i < getSize(args2); ++i){
-                c[i] = a[i] * b[0];
-            }
-        }else{
-#pragma omp parallel for
-            for(uint64_t i = 0; i < getSize(args2); ++i){
-                uint64_t o_index = i;//broadcast_o_index(args2->shape, args2->ndim, o_index);
-                uint64_t a_index = broadcast_i_index(args2->shape, o_index, args0->shape, args0->ndim, args2->ndim);
-                uint64_t b_index = broadcast_i_index(args2->shape, o_index, args1->shape, args1->ndim, args2->ndim);
-                c[i] = a[a_index] * b[b_index];
-            }
-        }
-
-#ifdef CVM_PROFILING
-        cvm_op_broadcast_cnt += omp_get_wtime() - start;
-#endif
-    print_to_file(args2, "broadcast_mul.txt");
-});
 
 
 /*
@@ -966,30 +856,6 @@ CVM_REGISTER_GLOBAL("cvm.runtime.cvm.abs")
     }
 });
 
-CVM_REGISTER_GLOBAL("cvm.runtime.cvm.broadcast_max")
-.set_body([](CVMArgs args, CVMRetValue *ret){
-    DLTensor *a = args[0];
-    DLTensor *b = args[1];
-    DLTensor *c = args[2];
-    int32_t *a_data = static_cast<int32_t*>(a->data);
-    int32_t* b_data = static_cast<int32_t*>(b->data);
-    int32_t* c_data = static_cast<int32_t*>(c->data);
-    if(getSize(b) == 1){
-#pragma omp parallel for
-      for(uint64_t i = 0; i < getSize(c); i++){
-        c_data[i] = a_data[i] > b_data[0] ? a_data[i] : b_data[0];
-      }
-    }else{
-#pragma omp parallel for
-      for(uint64_t i = 0; i < getSize(c); i++){
-        uint64_t o_index = i;//broadcast_o_index(c->shape, c->ndim, o_index);
-        uint64_t a_index = broadcast_i_index(c->shape, o_index, a->shape, a->ndim, c->ndim);
-        uint64_t b_index = broadcast_i_index(c->shape, o_index, b->shape, b->ndim, c->ndim);
-        //c_data[i] = (a_data[i] > b_data[i] ? a_data[i] : b_data[i]);
-        c_data[i] = a_data[a_index] > b_data[b_index] ? a_data[a_index] : b_data[b_index];
-      }
-    }
-});
 
 CVM_REGISTER_GLOBAL("cvm.runtime.cvm.concatenate")
 .set_body([](CVMArgs args, CVMRetValue *ret){
