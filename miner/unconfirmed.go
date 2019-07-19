6a40c9b9436adab1d0a5f5300bea0cf97ff4e2a8
ucwong <ucwong@126.com>
2019-07-19 18:29:36 +0800

    anchor

--- miner/unconfirmed.go
+++ miner/unconfirmed.go
@@ -104,7 +104,7 @@ func (set *unconfirmedBlocks) Shift(height uint64) {
 		case header == nil:
 			log.Warn("Failed to retrieve header of mined block", "number", next.index, "hash", next.hash)
 		case header.Hash() == next.hash:
-			log.Info("🚀 block canonic", "number", next.index, "hash", next.hash)
+			log.Info("⚓ block canonic", "number", next.index, "hash", next.hash)
 		default:
 			// Block is not canonical, check whether we have an uncle or a lost block
 			included := false
