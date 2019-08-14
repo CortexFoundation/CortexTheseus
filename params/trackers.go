dc0ac812b8d1b3a42fd636db7a7686a47c9e01d5
ucwong <ucwong@126.com>
2019-08-14 14:59:05 +0800

    dht domain added

--- params/trackers.go
+++ params/trackers.go
@@ -20,38 +20,50 @@ package params
 // the main Cortex network.
 var MainnetTrackers = []string{
 	"http://47.91.91.217:5008/announce",
-  "http://47.74.1.234:5008/announce",
-  "http://47.88.7.24:5008/announce",
-  "http://47.91.43.70:5008/announce",
+	"http://47.74.1.234:5008/announce",
+	"http://47.88.7.24:5008/announce",
+	"http://47.91.43.70:5008/announce",
 	"http://47.91.106.117:5008/announce",
 	"http://47.91.147.37:5008/announce",
-  "http://47.89.178.175:5008/announce",
-  "http://47.88.214.96:5008/announce",
-  "http://torrent.cortexlabs.ai:5008/announce",
-/*
-  "udp://47.91.91.217:5008/announce",
-	"udp://47.74.1.234:5008/announce",
-	"udp://47.88.7.24:5008/announce",
-	"udp://47.91.43.70:5008/announce",
-	"udp://47.91.106.117:5008/announce",
-	"udp://47.91.147.37:5008/announce",
-	"udp://47.89.178.175:5008/announce",
-	"udp://47.88.214.96:5008/announce",
-	"udp://torrent.cortexlabs.ai:5008/announce",
+	"http://47.89.178.175:5008/announce",
+	"http://47.88.214.96:5008/announce",
+	"http://torrent.cortexlabs.ai:5008/announce",
+	 "http://dht.coinbag.org:5008/announce",
+	 "http://dht.cortexlabs.ai:5008/announce",
+	/*
+	     "udp://47.91.91.217:5008/announce",
+	   	"udp://47.74.1.234:5008/announce",
+	   	"udp://47.88.7.24:5008/announce",
+	   	"udp://47.91.43.70:5008/announce",
+	   	"udp://47.91.106.117:5008/announce",
+	   	"udp://47.91.147.37:5008/announce",
+	   	"udp://47.89.178.175:5008/announce",
+	   	"udp://47.88.214.96:5008/announce",
+	   	"udp://torrent.cortexlabs.ai:5008/announce",
 
-	"ws://47.91.91.217:5008/announce",
-  "ws://47.74.1.234:5008/announce",
-  "ws://47.88.7.24:5008/announce",
-  "ws://47.91.43.70:5008/announce",
-  "ws://47.91.106.117:5008/announce",
-  "ws://47.91.147.37:5008/announce",
-  "ws://47.89.178.175:5008/announce",
-	"ws://47.88.214.96:5008/announce",
-	"ws://torrent.cortexlabs.ai:5008/announce",
-*/
+	   	"ws://47.91.91.217:5008/announce",
+	     "ws://47.74.1.234:5008/announce",
+	     "ws://47.88.7.24:5008/announce",
+	     "ws://47.91.43.70:5008/announce",
+	     "ws://47.91.106.117:5008/announce",
+	     "ws://47.91.147.37:5008/announce",
+	     "ws://47.89.178.175:5008/announce",
+	   	"ws://47.88.214.96:5008/announce",
+	   	"ws://torrent.cortexlabs.ai:5008/announce",
+	*/
 }
 
-var BernardTrackers = []string{}
+var BernardTrackers = []string{
+	"http://47.91.91.217:5008/announce",
+        "http://47.74.1.234:5008/announce",
+        "http://47.88.7.24:5008/announce",
+        "http://47.91.43.70:5008/announce",
+        "http://47.91.106.117:5008/announce",
+        "http://47.91.147.37:5008/announce",
+        "http://47.89.178.175:5008/announce",
+        "http://47.88.214.96:5008/announce",
+        "http://torrent.cortexlabs.ai:5008/announce",
+}
 
 var TorrentBoostNodes = []string{
 	"http://storage.cortexlabs.ai:7881",
