1c271d9ef1ada032ec1962c279bc040275aea487
ucwong <ucwong@126.com>
2019-08-15 18:46:15 +0800

    log show

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ function EXIT(){
 	exit 0
 }
 
-./cvm.sh > /dev/null 2>&1 &
+./cvm.sh & #> /dev/null 2>&1 &
 ./node.sh &
 while true; do
         server=`ps aux | grep 'cortex cvm' | grep -v grep | grep -v echo`
         if [ ! "$server" ]; then
-            ./cvm.sh > /dev/null 2>&1 &
+            ./cvm.sh & #> /dev/null 2>&1 &
         fi
         sleep 3
 done
