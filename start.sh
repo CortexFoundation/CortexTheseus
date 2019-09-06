ea6b2c165bbddb6106218d2099c84992650847d2
ucwong <ucwong@126.com>
2019-09-06 18:19:51 +0800

    [shell] quit

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ function EXIT(){
 	pkill -P `cat cvm.pid`
 	ps aux | grep 'cortex cvm' | grep -v grep | grep -v echo | cut -c 9-15 | xargs kill -9
 	pkill -P `cat tracker.pid`
+	pkill -P `cat node.pid`
 	#	ps aux | grep 'bittorrent-tracker' | grep -v grep | grep -v echo | cut -c 9-15 | xargs kill -9
 	exit 0
 }
@@ -12,6 +13,7 @@ function EXIT(){
 echo $! > cvm.pid
 ./tracker.sh &
 ./node.sh &
+echo $! > node.pid
 while true; do
         server=`ps aux | grep 'cortex cvm' | grep -v grep | grep -v echo`
         if [ ! "$server" ]; then
