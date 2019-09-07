de2e4bcdaf4eaa6e252e2f00e197ce6cc523a10d
ucwong <ucwong@126.com>
2019-09-08 01:18:34 +0800

    [shell] start tracker suc notification

--- tracker.sh
+++ tracker.sh
@@ -5,4 +5,6 @@ if [ -f ${tracker_pid} ];then
         exit 0
 fi
 ./build/bin/tracker > /dev/null 2>&1 &
+#echo $! > ${tracker_pid}
+echo "INFO [$(date +"%d-%m|%H:%M:%S:000")] Cortex tracker start with status $?."
 echo $! > ${tracker_pid}
