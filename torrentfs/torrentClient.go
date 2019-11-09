c17b963f886ea1df713fbb93f2cf83dd4122d49c
ucwong <ucwong@126.com>
2019-11-09 18:28:36 +0000

    1.8.0 stable

--- torrentfs/torrentClient.go
+++ torrentfs/torrentClient.go
@@ -816,7 +816,7 @@ func (tm *TorrentManager) listenTorrentProgress() {
 				}
 			}
 
-			if log_counter%20 == 0 && t.bytesCompleted > 0{
+			if log_counter%20 == 0 && t.bytesCompleted > 0 {
 				log.Info("[Downloading]", "hash", ih.String(), "complete", t.bytesCompleted, "quota", t.bytesRequested, "total", t.bytesMissing+t.bytesCompleted, "prog", math.Min(float64(t.bytesCompleted), float64(t.bytesRequested))/float64(t.bytesCompleted+t.bytesMissing), "seg", len(t.Torrent.PieceStateRuns()), "max", t.NumPieces(), "status", t.status, "boost", t.isBoosting)
 			}
 
