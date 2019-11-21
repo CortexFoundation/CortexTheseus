ea921b27766ce759f723d9b7c64867c05eace182
ucwong <ucwong@126.com>
2019-11-21 19:00:58 +0800

    miner/worker | add mining work interval from 2 to 3 seconds

--- miner/worker.go
+++ miner/worker.go
@@ -60,7 +60,7 @@ const (
 
 	// minRecommitInterval is the minimal time interval to recreate the mining block with
 	// any newly arrived transactions.
-	minRecommitInterval = 2 * time.Second
+	minRecommitInterval = 3 * time.Second
 
 	// maxRecommitInterval is the maximum time interval to recreate the mining block with
 	// any newly arrived transactions.
