951aecfa53d7c05996fcbfdd047368be4c7b82b5
ucwong <ucwong@126.com>
2019-12-02 15:10:14 +0800

    build/setenv.sh | remove unused cminer path export

--- solution/build/setenv.sh
+++ solution/build/setenv.sh
@@ -3,19 +3,19 @@
 # Curkoo algorithm dynamic library path
 OS=`uname`
 ROOT_DIR=`pwd`
-export LIB_MINER_DIR=$ROOT_DIR/cminer
+#export LIB_MINER_DIR=$ROOT_DIR/cminer
 echo $ROOT_DIR
 
 if [[ "$OS" == "Linux" ]]; then
-    export LD_LIBRARY_PATH=$LIB_MINER_DIR/:$LIB_MINER_DIR/cuckoo
-    export CGO_CFLAGS=-I$LIB_MINER_DIR/
-    export CGO_LDFLAGS=-L$LIB_MINER_DIR/\ -L$LIB_MINER_DIR/cuckoo
+    #export LD_LIBRARY_PATH=$LIB_MINER_DIR/:$LIB_MINER_DIR/cuckoo
+    #export CGO_CFLAGS=-I$LIB_MINER_DIR/
+    #export CGO_LDFLAGS=-L$LIB_MINER_DIR/\ -L$LIB_MINER_DIR/cuckoo
     echo $LD_LIBRARY_PATH
     echo $CGO_LDFLAGS
 elif [[ "$OS" == "Darwin" ]]; then
-	export DYLD_LIBRARY_PATH=$LIB_MINER_DIR/:$LIB_MINER_DIR/cuckoo
-    export CGO_CFLAGS=-I$LIB_MINER_DIR/
-    export CGO_LDFLAGS=-L$LIB_MINER_DIR/\ -L$LIB_MINER_DIR/cuckoo
+	#export DYLD_LIBRARY_PATH=$LIB_MINER_DIR/:$LIB_MINER_DIR/cuckoo
+    #export CGO_CFLAGS=-I$LIB_MINER_DIR/
+    #export CGO_LDFLAGS=-L$LIB_MINER_DIR/\ -L$LIB_MINER_DIR/cuckoo
 	echo $DYLD_LIBRARY_PATH
 else
 	echo "Sorry, cannot detected your OS" $OS
