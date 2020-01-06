22c18627b932111f3ac7734aaa4f2c1378857577
ucwong <ucwong@126.com>
2020-01-06 22:39:19 +0800

    vendor | tracker 1->5 mins

--- vendor/github.com/anacrolix/torrent/tracker_scraper.go
+++ vendor/github.com/anacrolix/torrent/tracker_scraper.go
@@ -98,7 +98,7 @@ func (me *trackerScraper) announce(event tracker.AnnounceEvent) (ret trackerAnno
 	defer func() {
 		ret.Completed = time.Now()
 	}()
-	ret.Interval = time.Minute
+	ret.Interval = time.Minute * 5
 	ip, err := me.getIp()
 	if err != nil {
 		ret.Err = fmt.Errorf("error getting ip: %s", err)
