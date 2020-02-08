e38b63e71c05251547da5f7ab790603a73baf85b
ucwong <ucwong@126.com>
2020-02-08 20:08:42 +0800

    fs/monitor, storage | fs storage current confirm status record

--- torrentfs/monitor.go
+++ torrentfs/monitor.go
@@ -1107,7 +1107,7 @@ func (m *Monitor) deal(block *Block) error {
 				log.Debug("Fs root version commit", "number", i, "root", m.fs.Root(), "elapsed", elapsed)
 			}
 
-			log.Info("Confirm to seal fs record", "number", i, "cap", len(m.taskCh), "record", record, "root", m.fs.Root().Hex())
+			log.Info("Confirm to seal fs record", "number", i, "cap", len(m.taskCh), "record", record, "root", m.fs.Root().Hex(), "blocks", len(m.fs.Blocks()), "files", len(m.fs.Files()))
 		} else {
 			if i%(batch/8) == 0 {
 				if storeErr := m.fs.WriteBlock(block, false); storeErr != nil {
