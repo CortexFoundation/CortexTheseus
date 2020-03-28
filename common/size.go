3070f319b3df31717a5e1cd8c8ba27ce955412fc
ucwong <ucwong@126.com>
2020-03-28 11:57:20 +0800

    common/size | add pretty string for hashrate

--- common/size.go
+++ common/size.go
@@ -54,3 +54,36 @@ func (s StorageSize) TerminalString() string {
 		return fmt.Sprintf("%.2fB", s)
 	}
 }
+
+type HashSize float64
+
+// String implements the stringer interface.
+func (s HashSize) String() string {
+	if s > 1099511627776 {
+		return fmt.Sprintf("%.2f TiH", s/1099511627776)
+	} else if s > 1073741824 {
+		return fmt.Sprintf("%.2f GiH", s/1073741824)
+	} else if s > 1048576 {
+		return fmt.Sprintf("%.2f MiH", s/1048576)
+	} else if s > 1024 {
+		return fmt.Sprintf("%.2f KiH", s/1024)
+	} else {
+		return fmt.Sprintf("%.2f H", s)
+	}
+}
+
+// TerminalString implements log.TerminalStringer, formatting a string for console
+// output during logging.
+func (s HashSize) TerminalString() string {
+	if s > 1099511627776 {
+		return fmt.Sprintf("%.2fTiH", s/1099511627776)
+	} else if s > 1073741824 {
+		return fmt.Sprintf("%.2fGiH", s/1073741824)
+	} else if s > 1048576 {
+		return fmt.Sprintf("%.2fMiH", s/1048576)
+	} else if s > 1024 {
+		return fmt.Sprintf("%.2fKiH", s/1024)
+	} else {
+		return fmt.Sprintf("%.2fH", s)
+	}
+}
