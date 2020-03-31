bf964c4a44cf8eae1f19523d0b4e01b7e07fd92e
ucwong <ucwong@126.com>
2020-03-31 15:28:06 +0800

    cmd/cortex/config | format

--- cmd/cortex/config.go
+++ cmd/cortex/config.go
@@ -239,20 +239,20 @@ func makeFullNode(ctx *cli.Context) *node.Node {
 	// }
 
 	// Whisper must be explicitly enabled by specifying at least 1 whisper flag or in dev mode
-        shhEnabled := enableWhisper(ctx)
-        //shhAutoEnabled := !ctx.GlobalIsSet(utils.WhisperEnabledFlag.Name) && ctx.GlobalIsSet(utils.DeveloperFlag.Name)
-        if shhEnabled {// || shhAutoEnabled {
-                if ctx.GlobalIsSet(utils.WhisperMaxMessageSizeFlag.Name) {
-                        cfg.Shh.MaxMessageSize = uint32(ctx.Int(utils.WhisperMaxMessageSizeFlag.Name))
-                }
-                if ctx.GlobalIsSet(utils.WhisperMinPOWFlag.Name) {
-                        cfg.Shh.MinimumAcceptedPOW = ctx.Float64(utils.WhisperMinPOWFlag.Name)
-                }
-                if ctx.GlobalIsSet(utils.WhisperRestrictConnectionBetweenLightClientsFlag.Name) {
-                        cfg.Shh.RestrictConnectionBetweenLightClients = true
-                }
-                utils.RegisterShhService(stack, &cfg.Shh)
-        }
+	shhEnabled := enableWhisper(ctx)
+	//shhAutoEnabled := !ctx.GlobalIsSet(utils.WhisperEnabledFlag.Name) && ctx.GlobalIsSet(utils.DeveloperFlag.Name)
+	if shhEnabled { // || shhAutoEnabled {
+		if ctx.GlobalIsSet(utils.WhisperMaxMessageSizeFlag.Name) {
+			cfg.Shh.MaxMessageSize = uint32(ctx.Int(utils.WhisperMaxMessageSizeFlag.Name))
+		}
+		if ctx.GlobalIsSet(utils.WhisperMinPOWFlag.Name) {
+			cfg.Shh.MinimumAcceptedPOW = ctx.Float64(utils.WhisperMinPOWFlag.Name)
+		}
+		if ctx.GlobalIsSet(utils.WhisperRestrictConnectionBetweenLightClientsFlag.Name) {
+			cfg.Shh.RestrictConnectionBetweenLightClients = true
+		}
+		utils.RegisterShhService(stack, &cfg.Shh)
+	}
 
 	// Add the Cortex Stats daemon if requested.
 	// if cfg.Cortexstats.URL != "" {
