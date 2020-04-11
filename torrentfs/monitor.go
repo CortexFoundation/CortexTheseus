d4ac6ede2819e7cccce5081909b9ce83b871eb18
ucwong <ucwong@126.com>
2020-04-11 12:33:18 +0800

    fs/monitor | more logs for first milestone

--- torrentfs/monitor.go
+++ torrentfs/monitor.go
@@ -1223,9 +1223,9 @@ func (m *Monitor) solve(block *types.Block) error {
 			if m.ckp != nil {
 				if m.ckp.TfsCheckPoint > 0 && i == m.ckp.TfsCheckPoint {
 					if m.fs.Root() == m.ckp.TfsRoot {
-						log.Info("First milestone", "number", i, "root", m.fs.Root(), "elapsed", elapsed)
+						log.Info("First milestone", "number", i, "root", m.fs.Root(), "blocks", len(m.fs.Blocks()), "txs", m.fs.Txs(), "files", len(m.fs.Files()), "elapsed", elapsed)
 					} else {
-						log.Error("Fs checkpoint failed", "number", i, "root", m.fs.Root(), "elapsed", elapsed, "exp", m.ckp.TfsRoot)
+						log.Error("Fs checkpoint failed", "number", i, "root", m.fs.Root(), "blocks", len(m.fs.Blocks()), "files", len(m.fs.Files()), "txs", m.fs.Txs(), "elapsed", elapsed, "exp", m.ckp.TfsRoot)
 						panic("Fs sync fatal error")
 						//m.lastNumber = 0
 						//m.fs.Reset()
