86e8760ad1b2efb1c993a5850eba99ed017899f1
ucwong <ucwong@126.com>
2020-04-21 15:02:57 +0800

    p2p,fs | remove some useless code

--- torrentfs/doc.go
+++ torrentfs/doc.go
@@ -2,7 +2,8 @@ package torrentfs
 
 const (
 	ProtocolName         = "nas"
-	ProtocolVersion      = 1
-	NumberOfMessageCodes = uint64(1048576)
+	ProtocolVersion      = uint64(1)
+	NumberOfMessageCodes = uint64(0)
 	ProtocolVersionStr   = "1.0"
+	statusCode           = 0
 )
--- whisper/whisperv6/whisper.go
+++ whisper/whisperv6/whisper.go
@@ -30,12 +30,12 @@ import (
 	"github.com/CortexFoundation/CortexTheseus/crypto"
 	"github.com/CortexFoundation/CortexTheseus/log"
 	"github.com/CortexFoundation/CortexTheseus/p2p"
-	"github.com/CortexFoundation/CortexTheseus/p2p/dnsdisc"
-	"github.com/CortexFoundation/CortexTheseus/params"
+	//"github.com/CortexFoundation/CortexTheseus/p2p/dnsdisc"
+	//"github.com/CortexFoundation/CortexTheseus/params"
 	"github.com/CortexFoundation/CortexTheseus/rlp"
 	"github.com/CortexFoundation/CortexTheseus/rpc"
-	mapset "github.com/ucwong/golang-set"
 	"github.com/syndtr/goleveldb/leveldb/errors"
+	mapset "github.com/ucwong/golang-set"
 	"golang.org/x/crypto/pbkdf2"
 	"golang.org/x/sync/syncmap"
 )
@@ -134,11 +134,11 @@ func New(cfg *Config) *Whisper {
 		},
 	}
 	//add dns discovery to whisper
-	client := dnsdisc.NewClient(dnsdisc.Config{})
-	s, err := client.NewIterator([]string{params.KnownDNSNetworks[params.MainnetGenesisHash]}...)
-	if err == nil {
-		whisper.protocol.DialCandidates = s
-	}
+	//client := dnsdisc.NewClient(dnsdisc.Config{})
+	//s, err := client.NewIterator([]string{params.KnownDNSNetworks[params.MainnetGenesisHash]}...)
+	//if err == nil {
+	//	whisper.protocol.DialCandidates = s
+	//}
 
 	return whisper
 }
