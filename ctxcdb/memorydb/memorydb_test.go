c1fc3f21da857f36decd821c301fdfae08d3e72a
hunter <929966834@qq.com>
2020-06-11 15:25:58 +0800

    fix memorydb test bug

--- ctxcdb/memorydb/memorydb_test.go
+++ ctxcdb/memorydb/memorydb_test.go
@@ -1,9 +1,14 @@
 package memorydb
 
+import (
+	"github.com/CortexFoundation/CortexTheseus/ctxcdb"
+	"github.com/CortexFoundation/CortexTheseus/ctxcdb/dbtest"
+	"testing"
+)
 
 func TestMemoryDB(t *testing.T) {
 	t.Run("DatabaseSuite", func(t *testing.T) {
-		dbtest.TestDatabaseSuite(t, func() ethdb.KeyValueStore {
+		dbtest.TestDatabaseSuite(t, func() ctxcdb.KeyValueStore {
 			return New()
 		})
 	})
