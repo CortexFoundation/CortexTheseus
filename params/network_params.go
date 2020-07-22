94f315f1ba9f9356b16f7fc290d76e680c39c436
ucwong <ucwong@126.com>
2020-07-22 14:00:44 +0800

    section update

--- params/network_params.go
+++ params/network_params.go
@@ -29,26 +29,46 @@ const (
 	BloomConfirms = 256
 
 	CHTFrequency = 32768
-	/*Check section:41 1376255
-	Check section:42 1409023
-	Check section:43 1441791
-	Check section:44 1474559
-	Check section:45 1507327
-	Check section:46 1540095
-	Check section:47 1572863
-	Check section:48 1605631
-	Check section:49 1638399
-	Check section:50 1671167
-	Check section:51 1703935
-	Check section:52 1736703
-	Check section:53 1769471
-	Check section:54 1802239
-	Check section:55 1835007
-	Check section:56 1867775
-	Check section:57 1900543
-	Check section:58 1933311
-	Check section:59 1966079
-	Check section:60 1998847
+	/*
+		Check section:60 1998847
+		Check section:61 2031615
+		Check section:62 2064383
+		Check section:63 2097151
+		Check section:64 2129919
+		Check section:65 2162687
+		Check section:66 2195455
+		Check section:67 2228223
+		Check section:68 2260991
+		Check section:69 2293759
+		Check section:70 2326527
+		Check section:71 2359295
+		Check section:72 2392063
+		Check section:73 2424831
+		Check section:74 2457599
+		Check section:75 2490367
+		Check section:76 2523135
+		Check section:77 2555903
+		Check section:78 2588671
+		Check section:79 2621439
+		Check section:80 2654207
+		Check section:81 2686975
+		Check section:82 2719743
+		Check section:83 2752511
+		Check section:84 2785279
+		Check section:85 2818047
+		Check section:86 2850815
+		Check section:87 2883583
+		Check section:88 2916351
+		Check section:89 2949119
+		Check section:90 2981887
+		Check section:91 3014655
+		Check section:92 3047423
+		Check section:93 3080191
+		Check section:94 3112959
+		Check section:95 3145727
+		Check section:96 3178495
+		Check section:97 3211263
+		Check section:98 3244031
 	*/
 	ImmutabilityThreshold = 90000
 )
