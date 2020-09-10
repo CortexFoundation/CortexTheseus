c9ba069fa9cc1d963d74bd136c2c7bbc62012fb3
ucwong <ucwong@126.com>
2020-09-10 10:30:45 +0800

    only roll back light sync if not fully validating

--- ctxc/downloader/downloader_test.go
+++ ctxc/downloader/downloader_test.go
@@ -1005,6 +1005,7 @@ func testShiftedHeaderAttack(t *testing.T, protocol int, mode SyncMode) {
 // sure no state was corrupted.
 func TestInvalidHeaderRollback63Fast(t *testing.T) { testInvalidHeaderRollback(t, 63, FastSync) }
 func TestInvalidHeaderRollback64Fast(t *testing.T) { testInvalidHeaderRollback(t, 64, FastSync) }
+func TestInvalidHeaderRollback65Fast(t *testing.T) { testInvalidHeaderRollback(t, 65, FastSync) }
 
 func testInvalidHeaderRollback(t *testing.T, protocol int, mode SyncMode) {
 	t.Parallel()
