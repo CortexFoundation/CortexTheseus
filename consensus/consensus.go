79ffea6566347bed6018f9e28f79efad69b1e4f4
ucwong <ucwong@126.com>
2020-12-16 11:30:49 +0800

    consensus interface fix

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -92,7 +92,7 @@ type Engine interface {
 	// consensus rules that happen at finalization (e.g. block rewards).
 	FinalizeAndAssemble(chain ChainHeaderReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error)
 
-	FinalizeWithoutParent(chain ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction,
+	FinalizeWithoutParent(chain ChainHeaderReader, header *types.Header, state *state.StateDB, txs []*types.Transaction,
 		uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error)
 	// Seal generates a new sealing request for the given input block and pushes
 	// the result into the given channel.
