e220c9f427c85c99cf6c37242332ee431815ee49
ucwong <ucwong@126.com>
2020-12-19 10:30:18 +0800

    Miner close

--- ctxc/backend.go
+++ ctxc/backend.go
@@ -576,7 +576,7 @@ func (s *Cortex) Stop() error {
 	s.bloomIndexer.Close()
 	close(s.closeBloomHandler)
 	s.txPool.Stop()
-	s.miner.Stop()
+	s.miner.Close()
 	s.blockchain.Stop()
 	s.engine.Close()
 	rawdb.PopUncleanShutdownMarker(s.chainDb)
