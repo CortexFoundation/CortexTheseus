7c104c10515ca622fdffbf4d08f2e8a783e622b1
ucwong <ucwong@126.com>
2021-01-21 10:58:44 +0800

    download test fix

--- ctxc/downloader/downloader_test.go
+++ ctxc/downloader/downloader_test.go
@@ -867,7 +867,7 @@ func testMultiProtoSync(t *testing.T, protocol int, mode SyncMode) {
 	assertOwnChain(t, tester, chain.len())
 
 	// Check that no peers have been dropped off
-	for _, version := range []int{64, 65} {
+	for _, version := range []int{64} {
 		peer := fmt.Sprintf("peer %d", version)
 		if _, ok := tester.peers[peer]; !ok {
 			t.Errorf("%s dropped", peer)
