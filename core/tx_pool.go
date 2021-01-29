4fa1f693ec4a966eb4e6ea27fdf76712eba56bfd
ucwong <ucwong@126.com>
2021-01-29 15:01:20 +0800

    return val

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -631,9 +631,11 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (replaced bool, err e
 	}
 	// Try to replace an existing transaction in the pending pool
 	from, _ := types.Sender(pool.signer, tx) // already validated
+
 	if blocked, _ := config.IsBlocked(from); blocked {
-		return
+		return false, errors.New("Freeze accounts")
 	}
+
 	if list := pool.pending[from]; list != nil && list.Overlaps(tx) {
 		// Nonce already pending, check if required price bump is met
 		inserted, old := list.Add(tx, pool.config.PriceBump)
