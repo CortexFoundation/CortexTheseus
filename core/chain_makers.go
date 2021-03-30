bcb587d35c142d3ff883fb5edcabd738b56f30de
ucwong <ucwong@126.com>
2021-03-30 11:40:40 +0800

    chain maker fix

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -285,10 +285,6 @@ func makeBlockChain(parent *types.Block, n int, engine consensus.Engine, db ctxc
 	return blocks
 }
 
-func (b *BlockGen) AddUncheckedTx(tx *types.Transaction) {
-	b.txs = append(b.txs, tx)
-}
-
 type fakeChainReader struct {
 	config *params.ChainConfig
 }
