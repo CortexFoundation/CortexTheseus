ff60056e90ca1e3a33d76876292a2e6e0c507c4e
ucwong <ucwong@126.com>
2021-09-11 09:41:41 +0800

    rt

--- vendor/github.com/anacrolix/chansync/set-once.go
+++ vendor/github.com/anacrolix/chansync/set-once.go
@@ -1,10 +1,15 @@
 package chansync
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // SetOnce is a boolean value that can only be flipped from false to true.
 type SetOnce struct {
-	ch        chan struct{}
+	ch chan struct{}
+	// Could be faster than trying to receive from ch
+	closed    uint32
 	initOnce  sync.Once
 	closeOnce sync.Once
 }
@@ -26,17 +31,12 @@ func (me *SetOnce) Set() (first bool) {
 	me.closeOnce.Do(func() {
 		me.init()
 		first = true
+		atomic.StoreUint32(&me.closed, 1)
 		close(me.ch)
 	})
 	return
 }
 
 func (me *SetOnce) IsSet() bool {
-	me.init()
-	select {
-	case <-me.ch:
-		return true
-	default:
-		return false
-	}
+	return atomic.LoadUint32(&me.closed) != 0
 }
