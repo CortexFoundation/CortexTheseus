045d11c1c52ec0460ddb4bbed795977ead134190
ucwong <ucwong@126.com>
2021-10-15 14:22:46 +0800

    helper test fix

--- ctxc/handler.go
+++ ctxc/handler.go
@@ -62,7 +62,7 @@ func errResp(code errCode, format string, v ...interface{}) error {
 }
 
 // txPool defines the methods needed from a transaction pool implementation to
-// support all the operations needed by the Ethereum chain protocols.
+// support all the operations needed by the Cortex chain protocols.
 type txPool interface {
 	// Has returns an indicator whether txpool has a transaction
 	// cached with the given hash.
--- ctxc/helper_test.go
+++ ctxc/helper_test.go
@@ -133,7 +133,7 @@ func (p *testTxPool) AddRemotes(txs []*types.Transaction) []error {
 }
 
 // Pending returns all the transactions known to the pool
-func (p *testTxPool) Pending() (map[common.Address]types.Transactions, error) {
+func (p *testTxPool) Pending(b bool) map[common.Address]types.Transactions {
 	p.lock.RLock()
 	defer p.lock.RUnlock()
 
@@ -145,7 +145,7 @@ func (p *testTxPool) Pending() (map[common.Address]types.Transactions, error) {
 	for _, batch := range batches {
 		sort.Sort(types.TxByNonce(batch))
 	}
-	return batches, nil
+	return batches
 }
 
 func (p *testTxPool) SubscribeNewTxsEvent(ch chan<- core.NewTxsEvent) event.Subscription {
