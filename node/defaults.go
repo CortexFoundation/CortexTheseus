18a7a747cc4880c82dd3247f4b19cbf0c734822d
ucwong <ucwong@126.com>
2023-02-13 04:13:09 +0800

    default node engine

--- node/defaults.go
+++ node/defaults.go
@@ -52,6 +52,7 @@ var DefaultConfig = Config{
 		MaxPeers:   50,
 		NAT:        nat.Any(),
 	},
+	DBEngine: "",
 }
 
 // DefaultDataDir is the default data directory to use for the databases and other
