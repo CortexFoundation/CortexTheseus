14b6b9708fcf6ee7959a384fe2163e2a67319bce
ucwong <ucwong@126.com>
2023-04-01 00:27:36 +0800

    consensus test opt

--- consensus/cuckoo/consensus_test.go
+++ consensus/cuckoo/consensus_test.go
@@ -10,44 +10,46 @@ import (
 )
 
 func TestReward(t *testing.T) {
-	reward := calculateRewardByNumber(big.NewInt(8409600-1), uint64(21))
-	t.Log(reward)
-	reward = calculateRewardByNumber(big.NewInt(8409600), uint64(21))
-	t.Log(reward)
-	reward = calculateRewardByNumber(big.NewInt(8409600*2), uint64(21))
-	t.Log(reward)
-	reward = calculateRewardByNumber(big.NewInt(8409600*3), uint64(21))
-	t.Log(reward)
-	reward = calculateRewardByNumber(big.NewInt(8409600*4), uint64(21))
-	t.Log(reward)
-	reward = calculateRewardByNumber(big.NewInt(8409600*5), uint64(21))
-	t.Log(reward)
-	reward = calculateRewardByNumber(big.NewInt(8409600*6), uint64(21))
-	t.Log(reward)
-	reward = calculateRewardByNumber(big.NewInt(8409600*7), uint64(21))
-	t.Log(reward)
+	for i := int64(1); i < 10; i++ {
+		reward := calculateRewardByNumber(big.NewInt(8409600*i-1), uint64(21))
+		t.Log("Period:", i, "reward:", reward)
+	}
 }
 
 func TestDifficultyCalculators(t *testing.T) {
 	rand.Seed(2)
 	diff := big.NewInt(10)
 	for i := 0; i < 30; i++ {
-
-		// 1 to 300 seconds diff
 		var timeDelta = uint64(18)
-		//rand.Read(difficulty)
+		if i > 5 {
+			timeDelta = uint64(8)
+		}
+
+		if i > 10 {
+			timeDelta = uint64(17)
+		}
+
+		if i > 15 {
+			timeDelta = uint64(13)
+		}
+
+		if i > 20 {
+			timeDelta = uint64(9)
+		}
+
+		if i > 25 {
+                        timeDelta = uint64(18)
+                }
+
 		header := &types.Header{
 			Difficulty: diff,
 			Number:     new(big.Int).SetUint64(rand.Uint64() % 50_000_000),
 			Time:       rand.Uint64(),
 		}
-		//if rand.Uint32()&1 == 0 {
 		header.UncleHash = types.EmptyUncleHash
-		//}
 
 		diff = calcDifficultyNeo(header.Time+timeDelta, header, true)
-		//t.Log(header.Difficulty)
-		t.Log(diff)
+		t.Log("interval:", timeDelta, "diff:", diff)
 	}
 }
 
