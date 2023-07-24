7342999e87d184998d2e106b6a1fdbc5828b1188
ucwong <ucwong@126.com>
2023-07-25 02:26:41 +0800

    state db fix

--- core/state/database.go
+++ core/state/database.go
@@ -76,6 +76,10 @@ type Trie interface {
 	// TryUpdateAccount abstract an account write in the trie.
 	TryUpdateAccount(key []byte, account *types.StateAccount) error
 
+	// UpdateContractCode abstracts code write to the trie. It is expected
+	// to be moved to the stateWriter interface when the latter is ready.
+	UpdateContractCode(address common.Address, codeHash common.Hash, code []byte) error
+
 	// TryUpdate associates key with value in the trie. If value has length zero, any
 	// existing value is deleted from the trie. The value bytes must not be modified
 	// by the caller while they are stored in the trie. If a node was not found in the
