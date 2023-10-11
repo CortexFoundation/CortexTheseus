cf0af541d815eed9f963c6e9a8b78293630129db
ucwong <ucwong@126.com>
2023-10-12 04:19:35 +0800

    add level file metrics

--- ctxcdb/pebble/pebble.go
+++ ctxcdb/pebble/pebble.go
@@ -58,19 +58,20 @@ type Database struct {
 	fn string     // filename for reporting
 	db *pebble.DB // Underlying pebble storage engine
 
-	compTimeMeter       metrics.Meter // Meter for measuring the total time spent in database compaction
-	compReadMeter       metrics.Meter // Meter for measuring the data read during compaction
-	compWriteMeter      metrics.Meter // Meter for measuring the data written during compaction
-	writeDelayNMeter    metrics.Meter // Meter for measuring the write delay number due to database compaction
-	writeDelayMeter     metrics.Meter // Meter for measuring the write delay duration due to database compaction
-	diskSizeGauge       metrics.Gauge // Gauge for tracking the size of all the levels in the database
-	diskReadMeter       metrics.Meter // Meter for measuring the effective amount of data read
-	diskWriteMeter      metrics.Meter // Meter for measuring the effective amount of data written
-	memCompGauge        metrics.Gauge // Gauge for tracking the number of memory compaction
-	level0CompGauge     metrics.Gauge // Gauge for tracking the number of table compaction in level0
-	nonlevel0CompGauge  metrics.Gauge // Gauge for tracking the number of table compaction in non0 level
-	seekCompGauge       metrics.Gauge // Gauge for tracking the number of table compaction caused by read opt
-	manualMemAllocGauge metrics.Gauge // Gauge for tracking amount of non-managed memory currently allocated
+	compTimeMeter       metrics.Meter   // Meter for measuring the total time spent in database compaction
+	compReadMeter       metrics.Meter   // Meter for measuring the data read during compaction
+	compWriteMeter      metrics.Meter   // Meter for measuring the data written during compaction
+	writeDelayNMeter    metrics.Meter   // Meter for measuring the write delay number due to database compaction
+	writeDelayMeter     metrics.Meter   // Meter for measuring the write delay duration due to database compaction
+	diskSizeGauge       metrics.Gauge   // Gauge for tracking the size of all the levels in the database
+	diskReadMeter       metrics.Meter   // Meter for measuring the effective amount of data read
+	diskWriteMeter      metrics.Meter   // Meter for measuring the effective amount of data written
+	memCompGauge        metrics.Gauge   // Gauge for tracking the number of memory compaction
+	level0CompGauge     metrics.Gauge   // Gauge for tracking the number of table compaction in level0
+	nonlevel0CompGauge  metrics.Gauge   // Gauge for tracking the number of table compaction in non0 level
+	seekCompGauge       metrics.Gauge   // Gauge for tracking the number of table compaction caused by read opt
+	manualMemAllocGauge metrics.Gauge   // Gauge for tracking amount of non-managed memory currently allocated
+	levelsGauge         []metrics.Gauge // Gauge for tracking the number of tables in levels
 
 	quitLock sync.RWMutex    // Mutex protecting the quit channel and the closed flag
 	quitChan chan chan error // Quit channel to stop the metrics collection before closing the database
@@ -228,7 +229,7 @@ func New(file string, cache int, handles int, namespace string, readonly bool, e
 	db.manualMemAllocGauge = metrics.NewRegisteredGauge(namespace+"memory/manualalloc", nil)
 
 	// Start up the metrics gathering and return
-	go db.meter(metricsGatheringInterval)
+	go db.meter(metricsGatheringInterval, namespace)
 	return db, nil
 }
 
@@ -425,7 +426,7 @@ func (d *Database) Path() string {
 
 // meter periodically retrieves internal pebble counters and reports them to
 // the metrics subsystem.
-func (d *Database) meter(refresh time.Duration) {
+func (d *Database) meter(refresh time.Duration, namespace string) {
 	var errc chan error
 	timer := time.NewTimer(refresh)
 	defer timer.Stop()
@@ -448,7 +449,7 @@ func (d *Database) meter(refresh time.Duration) {
 			compRead  int64
 			nWrite    int64
 
-			metrics            = d.db.Metrics()
+			stats              = d.db.Metrics()
 			compTime           = d.compTime.Load()
 			writeDelayCount    = d.writeDelayCount.Load()
 			writeDelayTime     = d.writeDelayTime.Load()
@@ -459,14 +460,14 @@ func (d *Database) meter(refresh time.Duration) {
 		writeDelayCounts[i%2] = writeDelayCount
 		compTimes[i%2] = compTime
 
-		for _, levelMetrics := range metrics.Levels {
+		for _, levelMetrics := range stats.Levels {
 			nWrite += int64(levelMetrics.BytesCompacted)
 			nWrite += int64(levelMetrics.BytesFlushed)
 			compWrite += int64(levelMetrics.BytesCompacted)
 			compRead += int64(levelMetrics.BytesRead)
 		}
 
-		nWrite += int64(metrics.WAL.BytesWritten)
+		nWrite += int64(stats.WAL.BytesWritten)
 
 		compWrites[i%2] = compWrite
 		compReads[i%2] = compRead
@@ -488,7 +489,7 @@ func (d *Database) meter(refresh time.Duration) {
 			d.compWriteMeter.Mark(compWrites[i%2] - compWrites[(i-1)%2])
 		}
 		if d.diskSizeGauge != nil {
-			d.diskSizeGauge.Update(int64(metrics.DiskSpaceUsage()))
+			d.diskSizeGauge.Update(int64(stats.DiskSpaceUsage()))
 		}
 		if d.diskReadMeter != nil {
 			d.diskReadMeter.Mark(0) // pebble doesn't track non-compaction reads
@@ -497,12 +498,20 @@ func (d *Database) meter(refresh time.Duration) {
 			d.diskWriteMeter.Mark(nWrites[i%2] - nWrites[(i-1)%2])
 		}
 		// See https://github.com/cockroachdb/pebble/pull/1628#pullrequestreview-1026664054
-		manuallyAllocated := metrics.BlockCache.Size + int64(metrics.MemTable.Size) + int64(metrics.MemTable.ZombieSize)
+		manuallyAllocated := stats.BlockCache.Size + int64(stats.MemTable.Size) + int64(stats.MemTable.ZombieSize)
 		d.manualMemAllocGauge.Update(manuallyAllocated)
-		d.memCompGauge.Update(metrics.Flush.Count)
+		d.memCompGauge.Update(stats.Flush.Count)
 		d.nonlevel0CompGauge.Update(nonLevel0CompCount)
 		d.level0CompGauge.Update(level0CompCount)
-		d.seekCompGauge.Update(metrics.Compact.ReadCount)
+		d.seekCompGauge.Update(stats.Compact.ReadCount)
+
+		for i, level := range stats.Levels {
+			// Append metrics for additional layers
+			if i >= len(d.levelsGauge) {
+				d.levelsGauge = append(d.levelsGauge, metrics.NewRegisteredGauge(namespace+fmt.Sprintf("tables/level%v", i), nil))
+			}
+			d.levelsGauge[i].Update(level.NumFiles)
+		}
 
 		// Sleep a bit, then repeat the stats collection
 		select {
