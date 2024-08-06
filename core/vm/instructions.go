ba65e9888f8e313287de9120c1729172d093172c
ucwong <ucwong@126.com>
2024-08-06 23:08:41 +0800

    core/vm: improved stack swap performance

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -595,180 +595,261 @@ func opGas(pc *uint64, interpreter *CVMInterpreter, callContext *ScopeContext) (
 	return nil, nil
 }
 
-/*func opInfer(pc *uint64, interpreter *CVMInterpreter, callContext *ScopeContext) ([]byte, error) {
-	_modelAddr, _inputAddr, _outputOffset := callContext.Stack.pop(), callContext.Stack.pop(), callContext.Stack.pop()
-	modelAddr := common.Address(_modelAddr.Bytes20())
-	inputAddr := common.Address(_inputAddr.Bytes20())
-	var (
-		modelMeta *torrentfs.ModelMeta
-		inputMeta *torrentfs.InputMeta
-	)
-	modelMeta, modelErr := checkModel(interpreter.cvm, callContext.Stack, modelAddr)
-	if modelErr != nil {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		return nil, modelErr
-	}
-
-	inputMeta, inputErr := checkInputMeta(interpreter.cvm, callContext.Stack, inputAddr)
-	if inputErr != nil {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		return nil, inputErr
-	}
-
-	log.Debug("interpreter check shape 1", "modelMeta", modelMeta, "inputMeta", inputMeta)
-	// Model&Input shape should match
-	if len(modelMeta.InputShape) != len(inputMeta.Shape) {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		if interpreter.cvm.vmConfig.DebugInferVM {
-			fmt.Println("modelmeta: ", modelMeta.InputShape, " inputmeta: ", inputMeta.Shape)
+/*
+	func opInfer(pc *uint64, interpreter *CVMInterpreter, callContext *ScopeContext) ([]byte, error) {
+		_modelAddr, _inputAddr, _outputOffset := callContext.Stack.pop(), callContext.Stack.pop(), callContext.Stack.pop()
+		modelAddr := common.Address(_modelAddr.Bytes20())
+		inputAddr := common.Address(_inputAddr.Bytes20())
+		var (
+			modelMeta *torrentfs.ModelMeta
+			inputMeta *torrentfs.InputMeta
+		)
+		modelMeta, modelErr := checkModel(interpreter.cvm, callContext.Stack, modelAddr)
+		if modelErr != nil {
+			callContext.Stack.push(new(uint256.Int).Clear())
+			return nil, modelErr
 		}
-		return nil, errMetaShapeNotMatch
-	}
-	log.Debug("interpreter check shape 2", "modelMeta", modelMeta, "inputMeta", inputMeta)
-	for idx, modelShape := range modelMeta.InputShape {
-		if modelShape != inputMeta.Shape[idx] || modelShape == 0 || inputMeta.Shape[idx] == 0 {
+
+		inputMeta, inputErr := checkInputMeta(interpreter.cvm, callContext.Stack, inputAddr)
+		if inputErr != nil {
+			callContext.Stack.push(new(uint256.Int).Clear())
+			return nil, inputErr
+		}
+
+		log.Debug("interpreter check shape 1", "modelMeta", modelMeta, "inputMeta", inputMeta)
+		// Model&Input shape should match
+		if len(modelMeta.InputShape) != len(inputMeta.Shape) {
 			callContext.Stack.push(new(uint256.Int).Clear())
 			if interpreter.cvm.vmConfig.DebugInferVM {
 				fmt.Println("modelmeta: ", modelMeta.InputShape, " inputmeta: ", inputMeta.Shape)
 			}
 			return nil, errMetaShapeNotMatch
 		}
-	}
-
-	//todo model & input tfs validation
-	output, err := interpreter.cvm.Infer(modelMeta.Hash.Hex(), inputMeta.Hash.Hex(), modelMeta.RawSize, inputMeta.RawSize)
-	if interpreter.cvm.vmConfig.DebugInferVM {
-		fmt.Println("DebugInferVM ", "output: ", output, " err: ", err, "model = ", modelMeta.Hash.Hex(), "input = ", inputMeta.Hash.Hex())
-	}
-	if err != nil {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		return nil, err
-	}
-	if err := callContext.Memory.WriteSolidityUint256Array(int64(_outputOffset.Uint64()), output); err != nil {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		return nil, err
-	}
-	callContext.Stack.push(new(uint256.Int).SetOne())
-
-	return nil, nil
-}
-
-func checkModel(cvm *CVM, stack *Stack, modelAddr common.Address) (*torrentfs.ModelMeta, error) {
-	var (
-		modelMeta *torrentfs.ModelMeta
-	)
-	var err error
-	if modelMeta, err = cvm.GetModelMeta(modelAddr); err != nil {
-		return nil, err
-	}
-	// Model Meta is validation
-	if cvm.StateDB.Uploading(modelAddr) {
-		return nil, errors.New("MODEL IS NOT UPLOADED ERROR")
-	}
-
-	matureBlockNumber := cvm.ChainConfig().GetMatureBlock()
-	log.Debug("checkModel", "modelAddr blocknum", cvm.StateDB.GetNum(modelAddr), "modelMeta", modelMeta)
-	if cvm.StateDB.GetNum(modelAddr).Int64() <= 0 {
-		return nil, errMetaInfoBlockNum
-	}
-	if cvm.StateDB.GetNum(modelAddr).Int64() > cvm.Context.BlockNumber.Int64()-matureBlockNumber {
-		log.Debug("instructions", "modelAddr", modelAddr, "modelAddrBlkNum", cvm.StateDB.GetNum(modelAddr), "Current", cvm.Context.BlockNumber, "MB", matureBlockNumber)
-		return nil, ErrMetaInfoNotMature
-	}
-
-	if cvm.StateDB.GetNum(modelAddr).Int64() < cvm.Context.BlockNumber.Int64()-params.ExpiredBlks {
-		return nil, errMetaInfoExpired
-	}
-
-	if modelMeta.Gas > params.MODEL_GAS_LIMIT {
-		//return nil, errExecutionReverted
-		return nil, errors.New("INVALID MODEL GAS LIMIT ERROR")
-	}
-	return modelMeta, nil
-}
-
-func checkInputMeta(cvm *CVM, stack *Stack, inputAddr common.Address) (*torrentfs.InputMeta, error) {
-	var (
-		inputMeta *torrentfs.InputMeta
-		err       error
-	)
-	if inputMeta, err = cvm.GetInputMeta(inputAddr); err != nil {
-		return nil, err
-	}
-	// Model Meta is validation
-	if cvm.StateDB.Uploading(inputAddr) {
-		return nil, errors.New("MODEL IS NOT UPLOADED ERROR")
-	}
-
-	log.Debug("checkInput", "modelAddr blocknum", cvm.StateDB.GetNum(inputAddr), "inputMeta", inputMeta)
-	if cvm.StateDB.GetNum(inputAddr).Int64() <= 0 {
-		return nil, errMetaInfoBlockNum
-	}
-
-	matureBlockNumber := cvm.ChainConfig().GetMatureBlock()
-	if cvm.StateDB.GetNum(inputAddr).Int64() > cvm.Context.BlockNumber.Int64()-matureBlockNumber {
-		log.Debug("instructions", "inputAddr", inputAddr, "inputAddrBlkNum", cvm.StateDB.GetNum(inputAddr), "Current", cvm.Context.BlockNumber, "Uploading", cvm.StateDB.Uploading(inputAddr), "MB", matureBlockNumber)
-		return nil, ErrMetaInfoNotMature
-	}
-
-	if cvm.StateDB.GetNum(inputAddr).Int64() < cvm.Context.BlockNumber.Int64()-params.ExpiredBlks {
-		return nil, errMetaInfoExpired
-	}
-
-	return inputMeta, nil
-}
-
-func opInferArray(pc *uint64, interpreter *CVMInterpreter, callContext *ScopeContext) ([]byte, error) {
-	_modelAddr, _inputHeaderOffset, _outputOffset := callContext.Stack.pop(), callContext.Stack.pop(), callContext.Stack.pop()
-	// fmt.Println(fmt.Sprintf("%d, %d, %d", _modelAddr, _inputHeaderOffset, _outputOffset))
-	inputBuff, inputError := interpreter.cvm.StateDB.GetSolidityBytes(callContext.Contract.Address(), common.Hash(_inputHeaderOffset.Bytes32()))
-	if inputError != nil {
-		return nil, inputError
-	}
-	inputSize := uint256.NewInt(uint64(len(inputBuff)))
-	modelAddr := common.Address(_modelAddr.Bytes20())
-	// log.Debug(fmt.Sprintf("_input = %v, payload = %v ", inputSize, inputBuff))
-
-	modelMeta, modelErr := checkModel(interpreter.cvm, callContext.Stack, modelAddr)
-	if modelErr != nil {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		return nil, modelErr
-	}
-
-	if false {
-		//TODO(tian) omit input shape for infer array
-		var dataSize uint64 = 1
-		for _, modelShape := range modelMeta.InputShape {
-			dataSize *= modelShape
-		}
-		if dataSize != inputSize.Uint64() {
-			callContext.Stack.push(new(uint256.Int).Clear())
-			if interpreter.cvm.vmConfig.DebugInferVM {
-				fmt.Println("modelmeta: ", modelMeta.InputShape, "datasize: ", dataSize, "inputSize: ", inputSize)
+		log.Debug("interpreter check shape 2", "modelMeta", modelMeta, "inputMeta", inputMeta)
+		for idx, modelShape := range modelMeta.InputShape {
+			if modelShape != inputMeta.Shape[idx] || modelShape == 0 || inputMeta.Shape[idx] == 0 {
+				callContext.Stack.push(new(uint256.Int).Clear())
+				if interpreter.cvm.vmConfig.DebugInferVM {
+					fmt.Println("modelmeta: ", modelMeta.InputShape, " inputmeta: ", inputMeta.Shape)
+				}
+				return nil, errMetaShapeNotMatch
 			}
-			return nil, errMetaShapeNotMatch
 		}
-	}
-	var output []byte
-	var err error
-	output, err = interpreter.cvm.InferArray(modelMeta.Hash.Hex(),
-		inputBuff, modelMeta.RawSize)
-	if err != nil {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		return nil, err
-	}
-	if interpreter.cvm.vmConfig.DebugInferVM {
-		fmt.Println("output", output)
-	}
-	if err := callContext.Memory.WriteSolidityUint256Array(int64(_outputOffset.Uint64()), output); err != nil {
-		callContext.Stack.push(new(uint256.Int).Clear())
-		return nil, err
-	}
-	// interpreter.intPool.get().SetUint64
-	callContext.Stack.push(new(uint256.Int).SetOne())
 
+		//todo model & input tfs validation
+		output, err := interpreter.cvm.Infer(modelMeta.Hash.Hex(), inputMeta.Hash.Hex(), modelMeta.RawSize, inputMeta.RawSize)
+		if interpreter.cvm.vmConfig.DebugInferVM {
+			fmt.Println("DebugInferVM ", "output: ", output, " err: ", err, "model = ", modelMeta.Hash.Hex(), "input = ", inputMeta.Hash.Hex())
+		}
+		if err != nil {
+			callContext.Stack.push(new(uint256.Int).Clear())
+			return nil, err
+		}
+		if err := callContext.Memory.WriteSolidityUint256Array(int64(_outputOffset.Uint64()), output); err != nil {
+			callContext.Stack.push(new(uint256.Int).Clear())
+			return nil, err
+		}
+		callContext.Stack.push(new(uint256.Int).SetOne())
+
+		return nil, nil
+	}
+
+	func checkModel(cvm *CVM, stack *Stack, modelAddr common.Address) (*torrentfs.ModelMeta, error) {
+		var (
+			modelMeta *torrentfs.ModelMeta
+		)
+		var err error
+		if modelMeta, err = cvm.GetModelMeta(modelAddr); err != nil {
+			return nil, err
+		}
+		// Model Meta is validation
+		if cvm.StateDB.Uploading(modelAddr) {
+			return nil, errors.New("MODEL IS NOT UPLOADED ERROR")
+		}
+
+		matureBlockNumber := cvm.ChainConfig().GetMatureBlock()
+		log.Debug("checkModel", "modelAddr blocknum", cvm.StateDB.GetNum(modelAddr), "modelMeta", modelMeta)
+		if cvm.StateDB.GetNum(modelAddr).Int64() <= 0 {
+			return nil, errMetaInfoBlockNum
+		}
+		if cvm.StateDB.GetNum(modelAddr).Int64() > cvm.Context.BlockNumber.Int64()-matureBlockNumber {
+			log.Debug("instructions", "modelAddr", modelAddr, "modelAddrBlkNum", cvm.StateDB.GetNum(modelAddr), "Current", cvm.Context.BlockNumber, "MB", matureBlockNumber)
+			return nil, ErrMetaInfoNotMature
+		}
+
+		if cvm.StateDB.GetNum(modelAddr).Int64() < cvm.Context.BlockNumber.Int64()-params.ExpiredBlks {
+			return nil, errMetaInfoExpired
+		}
+
+		if modelMeta.Gas > params.MODEL_GAS_LIMIT {
+			//return nil, errExecutionReverted
+			return nil, errors.New("INVALID MODEL GAS LIMIT ERROR")
+		}
+		return modelMeta, nil
+	}
+
+	func checkInputMeta(cvm *CVM, stack *Stack, inputAddr common.Address) (*torrentfs.InputMeta, error) {
+		var (
+			inputMeta *torrentfs.InputMeta
+			err       error
+		)
+		if inputMeta, err = cvm.GetInputMeta(inputAddr); err != nil {
+			return nil, err
+		}
+		// Model Meta is validation
+		if cvm.StateDB.Uploading(inputAddr) {
+			return nil, errors.New("MODEL IS NOT UPLOADED ERROR")
+		}
+
+		log.Debug("checkInput", "modelAddr blocknum", cvm.StateDB.GetNum(inputAddr), "inputMeta", inputMeta)
+		if cvm.StateDB.GetNum(inputAddr).Int64() <= 0 {
+			return nil, errMetaInfoBlockNum
+		}
+
+		matureBlockNumber := cvm.ChainConfig().GetMatureBlock()
+		if cvm.StateDB.GetNum(inputAddr).Int64() > cvm.Context.BlockNumber.Int64()-matureBlockNumber {
+			log.Debug("instructions", "inputAddr", inputAddr, "inputAddrBlkNum", cvm.StateDB.GetNum(inputAddr), "Current", cvm.Context.BlockNumber, "Uploading", cvm.StateDB.Uploading(inputAddr), "MB", matureBlockNumber)
+			return nil, ErrMetaInfoNotMature
+		}
+
+		if cvm.StateDB.GetNum(inputAddr).Int64() < cvm.Context.BlockNumber.Int64()-params.ExpiredBlks {
+			return nil, errMetaInfoExpired
+		}
+
+		return inputMeta, nil
+	}
+
+	func opInferArray(pc *uint64, interpreter *CVMInterpreter, callContext *ScopeContext) ([]byte, error) {
+		_modelAddr, _inputHeaderOffset, _outputOffset := callContext.Stack.pop(), callContext.Stack.pop(), callContext.Stack.pop()
+		// fmt.Println(fmt.Sprintf("%d, %d, %d", _modelAddr, _inputHeaderOffset, _outputOffset))
+		inputBuff, inputError := interpreter.cvm.StateDB.GetSolidityBytes(callContext.Contract.Address(), common.Hash(_inputHeaderOffset.Bytes32()))
+		if inputError != nil {
+			return nil, inputError
+		}
+		inputSize := uint256.NewInt(uint64(len(inputBuff)))
+		modelAddr := common.Address(_modelAddr.Bytes20())
+		// log.Debug(fmt.Sprintf("_input = %v, payload = %v ", inputSize, inputBuff))
+
+		modelMeta, modelErr := checkModel(interpreter.cvm, callContext.Stack, modelAddr)
+		if modelErr != nil {
+			callContext.Stack.push(new(uint256.Int).Clear())
+			return nil, modelErr
+		}
+
+		if false {
+			//TODO(tian) omit input shape for infer array
+			var dataSize uint64 = 1
+			for _, modelShape := range modelMeta.InputShape {
+				dataSize *= modelShape
+			}
+			if dataSize != inputSize.Uint64() {
+				callContext.Stack.push(new(uint256.Int).Clear())
+				if interpreter.cvm.vmConfig.DebugInferVM {
+					fmt.Println("modelmeta: ", modelMeta.InputShape, "datasize: ", dataSize, "inputSize: ", inputSize)
+				}
+				return nil, errMetaShapeNotMatch
+			}
+		}
+		var output []byte
+		var err error
+		output, err = interpreter.cvm.InferArray(modelMeta.Hash.Hex(),
+			inputBuff, modelMeta.RawSize)
+		if err != nil {
+			callContext.Stack.push(new(uint256.Int).Clear())
+			return nil, err
+		}
+		if interpreter.cvm.vmConfig.DebugInferVM {
+			fmt.Println("output", output)
+		}
+		if err := callContext.Memory.WriteSolidityUint256Array(int64(_outputOffset.Uint64()), output); err != nil {
+			callContext.Stack.push(new(uint256.Int).Clear())
+			return nil, err
+		}
+		// interpreter.intPool.get().SetUint64
+		callContext.Stack.push(new(uint256.Int).SetOne())
+
+		return nil, nil
+	}
+*/
+func opSwap1(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap1()
 	return nil, nil
-}*/
+}
+
+func opSwap2(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap2()
+	return nil, nil
+}
+
+func opSwap3(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap3()
+	return nil, nil
+}
+
+func opSwap4(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap4()
+	return nil, nil
+}
+
+func opSwap5(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap5()
+	return nil, nil
+}
+
+func opSwap6(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap6()
+	return nil, nil
+}
+
+func opSwap7(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap7()
+	return nil, nil
+}
+
+func opSwap8(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap8()
+	return nil, nil
+}
+
+func opSwap9(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap9()
+	return nil, nil
+}
+
+func opSwap10(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap10()
+	return nil, nil
+}
+
+func opSwap11(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap11()
+	return nil, nil
+}
+
+func opSwap12(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap12()
+	return nil, nil
+}
+
+func opSwap13(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap13()
+	return nil, nil
+}
+
+func opSwap14(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap14()
+	return nil, nil
+}
+
+func opSwap15(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap15()
+	return nil, nil
+}
+
+func opSwap16(pc *uint64, interpreter *CVMInterpreter, scope *ScopeContext) ([]byte, error) {
+	scope.Stack.swap16()
+	return nil, nil
+}
 
 func opCreate(pc *uint64, interpreter *CVMInterpreter, callContext *ScopeContext) ([]byte, error) {
 	if interpreter.readOnly {
--- core/vm/runtime/runtime_test.go
+++ core/vm/runtime/runtime_test.go
@@ -470,3 +470,32 @@ func BenchmarkSimpleLoop(b *testing.B) {
 	//benchmarkNonModifyingCode(10000000, staticCallIdentity, "staticcall-identity-10M", b)
 	//benchmarkNonModifyingCode(10000000, loopingCode, "loop-10M", b)
 }
+
+func BenchmarkVM_SWAP1(b *testing.B) {
+	// returns a contract that does n swaps (SWAP1)
+	swapContract := func(n uint64) []byte {
+		contract := []byte{
+			byte(vm.PUSH0), // PUSH0
+			byte(vm.PUSH0), // PUSH0
+		}
+		for i := uint64(0); i < n; i++ {
+			contract = append(contract, byte(vm.SWAP1))
+		}
+		return contract
+	}
+
+	state, _ := state.New(types.EmptyRootHash, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
+	contractAddr := common.BytesToAddress([]byte("contract"))
+
+	b.Run("10k", func(b *testing.B) {
+		contractCode := swapContract(10_000)
+		state.SetCode(contractAddr, contractCode)
+
+		for i := 0; i < b.N; i++ {
+			_, _, err := Call(contractAddr, []byte{}, &Config{State: state})
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+}
