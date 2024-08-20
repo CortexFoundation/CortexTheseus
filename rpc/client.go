337938a571b439d17f23f0b73d6930df6d726112
ucwong <ucwong@126.com>
2024-08-20 22:12:05 +0800

    rpc: add timeout to rpc client Unsubscribe

--- rpc/client.go
+++ rpc/client.go
@@ -41,7 +41,8 @@ var (
 const (
 	// Timeouts
 	defaultDialTimeout = 10 * time.Second // used if context has no deadline
-	subscribeTimeout   = 5 * time.Second  // overall timeout ctxc_subscribe, rpc_modules calls
+	subscribeTimeout   = 10 * time.Second // overall timeout eth_subscribe, rpc_modules calls
+	unsubscribeTimeout = 10 * time.Second // timeout for *_unsubscribe calls
 )
 
 const (
