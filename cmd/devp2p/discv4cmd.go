1afe50ff85f2ab9c7788b3841dc3de36d9bfc77b
Chen Kai <281165273grape@gmail.com>
2025-03-13 15:16:01 +0100

    p2p/discover: expose discv5 functions for portal JSON-RPC interface (#31117)
    Fixes #31093
    
    Here we add some API functions on the UDPv5 object for the purpose of implementing
    the Portal Network JSON-RPC API in the shisui client.
    
    ---------
    
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- cmd/devp2p/discv4cmd.go
+++ cmd/devp2p/discv4cmd.go
@@ -148,7 +148,7 @@ func discv4Ping(ctx *cli.Context) error {
 	defer disc.Close()
 
 	start := time.Now()
-	if err := disc.Ping(n); err != nil {
+	if _, err := disc.Ping(n); err != nil {
 		return fmt.Errorf("node didn't respond: %v", err)
 	}
 	fmt.Printf("node responded to ping (RTT %v).\n", time.Since(start))
