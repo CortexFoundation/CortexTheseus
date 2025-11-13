15a98177ccc83ad18394e45a804d6efaccb8888f
phrwlk <phrwlk7@gmail.com>
2025-11-13 10:17:54 +0200

    accounts/keystore: clear decrypted key after use (#33090)

--- accounts/keystore/keystore.go
+++ accounts/keystore/keystore.go
@@ -427,6 +427,7 @@ func (ks *KeyStore) Export(a accounts.Account, passphrase, newPassphrase string)
 	if err != nil {
 		return nil, err
 	}
+	defer zeroKey(key.PrivateKey)
 	var N, P int
 	if store, ok := ks.storage.(*keyStorePassphrase); ok {
 		N, P = store.scryptN, store.scryptP
@@ -480,6 +481,7 @@ func (ks *KeyStore) Update(a accounts.Account, passphrase, newPassphrase string)
 	if err != nil {
 		return err
 	}
+	defer zeroKey(key.PrivateKey)
 	return ks.storage.StoreKey(a.URL.Path, key, newPassphrase)
 }
 
