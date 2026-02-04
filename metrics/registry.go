fdf9eab7c55b3c8923895484608676eac651f721
ucwong <ucwong@126.com>
2026-02-04 21:38:53 +0800

    add missing GaugeInfo

--- metrics/registry.go
+++ metrics/registry.go
@@ -157,6 +157,8 @@ func (r *StandardRegistry) GetAll() map[string]map[string]any {
 			values["value"] = metric.Snapshot().Value()
 		case GaugeFloat64:
 			values["value"] = metric.Snapshot().Value()
+		case GaugeInfo:
+			values["value"] = metric.Snapshot().Value()
 		case Healthcheck:
 			values["error"] = nil
 			metric.Check()
