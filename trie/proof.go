bcb81a95968e45556633723471f4486f23dd67b6
ucwong <ucwong@126.com>
2026-03-06 02:13:47 +0800

    validate proof last key fix

--- trie/proof.go
+++ trie/proof.go
@@ -529,7 +529,6 @@ func VerifyRangeProof(rootHash common.Hash, firstKey []byte, lastKey []byte, key
 	}
 	// Special case, there is only one element and two edge keys are same.
 	// In this case, we can't construct two edge paths. So handle it here.
-	lastKey = keys[len(keys)-1]
 	if len(keys) == 1 && bytes.Equal(firstKey, lastKey) {
 		root, val, err := proofToPath(rootHash, nil, firstKey, notary, false)
 		if err != nil {
